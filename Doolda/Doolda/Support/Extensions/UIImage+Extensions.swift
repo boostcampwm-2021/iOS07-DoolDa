1c588ff2c425cded04fb76703bdd92734277ff49
yabby <yabby1997@gmail.com>
2021-11-02 15:20:50 +0900

    (#9) ✨: systemImage를 위한 UIImage extension 생성

---
+++ Doolda/Doolda/Support/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIImage+Extensions.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/02.
+//
+
+import UIKit
+
+extension UIImage {
+    static let refresh = UIImage(systemName: "arrow.clockwise")
+}
