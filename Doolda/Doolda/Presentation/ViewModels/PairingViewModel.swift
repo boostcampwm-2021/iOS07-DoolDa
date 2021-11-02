5ce473c6408dfcda6614fcad22f6670b09d10037
Jung Ji Seung <rlsmd9@naver.com>
2021-11-02 13:22:06 +0900

    (#40) ✨: PairingViewModel 생성 및 myId 주입 구현

---
+++ Doolda/Doolda/Presentation/ViewModels/PairingViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PairingViewModel.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/02.
+//
+
+import Foundation
+
+final class PairingViewModel {
+    private let myId: UUID
+    
+    init(myId: UUID) {
+        self.myId = myId
+    }
+}
