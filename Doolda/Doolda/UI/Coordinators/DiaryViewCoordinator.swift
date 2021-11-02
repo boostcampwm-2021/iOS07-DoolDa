6f4650bc22ba85b957c677c23fcf49325599d404
ehWld <729psw@gmail.com>
2021-11-02 19:25:20 +0900

    (#25, #33) ✨: SplashViewCoordinator delegate 구현

---
+++ Doolda/Doolda/UI/Coordinators/DiaryViewCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  DiaryViewCoordinator.swift
+//  Doolda
+//
+//  Created by Dozzing on 2021/11/02.
+//
+
+import Foundation
+
+class DiaryViewCoordinator: Coordinator {
+    func start() {
+    }
+}
