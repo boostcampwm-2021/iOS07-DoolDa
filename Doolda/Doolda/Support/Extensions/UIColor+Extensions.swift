f2f23226125a067bf27f3f470e8434ea898abbf9
yabby <yabby1997@gmail.com>
2021-11-02 14:33:40 +0900

    (#9) ✨: dooldaLabel, dooldaSubLabel 컬러 추가

---
+++ Doolda/Doolda/Support/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extensions.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/02.
+//
+
+import UIKit
+
+extension UIColor {
+    static let dooldaLabel = UIColor(named: "dooldaLabel")
+    static let dooldaSubLabel = UIColor(named: "dooldaSubLabel")
+}
