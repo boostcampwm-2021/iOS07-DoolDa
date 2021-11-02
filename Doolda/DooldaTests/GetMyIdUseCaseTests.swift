1576b78af27cfca3cf1a5372caa971a99ce7f910
MINJUKIM <alsxm553@gmail.com>
2021-11-02 15:27:33 +0900

    (#38) 🧪: getMyId 에러 리턴 테스트 추가

--- Doolda/DooldaTests/GetMyIdUseCaseTests.swift
+++ Doolda/DooldaTests/GetMyIdUseCaseTests.swift
@@ -58,4 +58,51 @@ class GetMyIdUseCaseTests: XCTestCase {
         }.cancel()
     }
     
+    func testGetMyIdError() throws {
+        enum DummyError: Error {
+            case dummyError
+        }
+        
+        class DummyRepository: UserRepositoryProtocol {
+            func fetchMyId() -> AnyPublisher<String, Error> {
+                return Result.Publisher(DummyError.dummyError).eraseToAnyPublisher()
+            }
+            
+            func fetchPairId() -> AnyPublisher<String, Error> {
+                return Result.Publisher("DummyPairId").eraseToAnyPublisher()
+            }
+            
+            func saveMyId(_ id: String) {}
+            
+            func savePairId(_ id: String) {}
+            
+            func getGlobalFont() -> String {
+                return "font"
+            }
+        }
+        
+        self.dummyRepository = DummyRepository()
+        if let repository = self.dummyRepository {
+            self.getMyIdUseCase = GetMyIdUseCase(userRepository: repository)
+        }
+        
+        guard let usecase = self.getMyIdUseCase else {
+            XCTFail()
+            return
+        }
+        usecase.getMyId().sink { completion in
+            switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                guard let dummyError = error as? DummyError else {
+                    XCTFail("전달되는 error 값이 예상 error값과 다릅니다.")
+                    return
+                }
+                XCTAssertEqual(DummyError.dummyError, dummyError, "전달되는 error 값이 예상 error값과 다릅니다.")
+            }
+        } receiveValue: { id in
+            XCTFail()
+        }.cancel()
+    }
 }
