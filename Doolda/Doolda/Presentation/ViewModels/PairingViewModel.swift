c77d3b217ddb1192ab15d70507f87ae9a15250ee
Jung Ji Seung <rlsmd9@naver.com>
2021-11-02 13:55:12 +0900

    (#42) ✨: 상대방 식별코드가 유효한 UUID 포맷인지 검증하도록 구현

--- Doolda/Doolda/Presentation/ViewModels/PairingViewModel.swift
+++ Doolda/Doolda/Presentation/ViewModels/PairingViewModel.swift
@@ -5,12 +5,53 @@
 //  Created by 정지승 on 2021/11/02.
 //
 
+import Combine
 import Foundation
 
-final class PairingViewModel {
-    private let myId: UUID
+enum PairingViewModelError: LocalizedError {
+    case friendIdIsNotInvalid
+    case friendIdIsEmpty
+    
+    var errorDescription: String? {
+        switch self {
+        case .friendIdIsNotInvalid:
+            return "유효하지 않은 친구 ID 입니다."
+        case .friendIdIsEmpty:
+            return "친구 ID가 비어있습니다."
+        }
+    }
+}
+
+protocol PairingViewModelInput {
+    func changeFriend(id: String)
+}
+
+protocol PairingViewModelOutput {
+    var errorPublisher: Published<Error?>.Publisher { get }
+}
+
+typealias PairingViewModelProtocol = PairingViewModelInput & PairingViewModelOutput
+
+final class PairingViewModel: PairingViewModelProtocol {
+    let myId: UUID
+    
+    var friendId: UUID?
+    var errorPublisher: Published<Error?>.Publisher { $error }
+    @Published var error: Error?
     
     init(myId: UUID) {
         self.myId = myId
+    }
+    
+    func changeFriend(id: String) {
+        self.friendId = UUID(uuidString: id)
+        
+        if id.isEmpty {
+            self.error = PairingViewModelError.friendIdIsEmpty
+        } else if self.friendId == nil {
+            self.error = PairingViewModelError.friendIdIsNotInvalid
+        } else {
+            self.error = nil
+        }
     }
 }
