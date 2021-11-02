b3ea036e021a1ad89894adb06f591534ffa3b267
MINJUKIM <alsxm553@gmail.com>
2021-11-02 13:29:06 +0900

    (#38) ✨: UserRepositoryProtocol Interface 구현

---
+++ Doolda/Doolda/Domain/Interfaces/UserRepositoryProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  UserRepositoryProtocol.swift
+//  Doolda
+//
+//  Created by 김민주 on 2021/11/02.
+//
+
+import Foundation
+import Combine
+
+protocol UserRepositoryProtocol {
+    func fetchMyId() -> AnyPublisher<String, Error>
+    func fetchPairId() -> AnyPublisher<String, Error>
+
+    func save(myId: String)
+    func save(pairId: String)
+    
+    func getGlobalFont() -> String
+}
