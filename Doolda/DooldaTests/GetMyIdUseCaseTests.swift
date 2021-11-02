0ac004a860026574bb8f3b2144eefbc7b1b7f92e
MINJUKIM <alsxm553@gmail.com>
2021-11-02 15:27:06 +0900

    (#38) 🧪: getMyId 값 리턴 테스트 추가

---
+++ Doolda/DooldaTests/GetMyIdUseCaseTests.swift
@@ -0,0 +1,61 @@
+//
+//  GetMyIdUseCaseTests.swift
+//  DooldaTests
+//
+//  Created by 김민주 on 2021/11/02.
+//
+
+import XCTest
+import Combine
+
+class GetMyIdUseCaseTests: XCTestCase {
+    private var dummyRepository: UserRepositoryProtocol?
+    private var getMyIdUseCase: GetMyIdUseCase?
+
+    override func tearDownWithError() throws {
+        self.getMyIdUseCase = nil
+        self.dummyRepository = nil
+    }
+
+    func testGetMyIdSuceess() throws {
+        
+        class DummyRepository: UserRepositoryProtocol {
+            func fetchMyId() -> AnyPublisher<String, Error> {
+                return Result.Publisher("00000000-0000-0000-0000-000000000001").eraseToAnyPublisher()
+            }
+            
+            func fetchPairId() -> AnyPublisher<String, Error> {
+                return Result.Publisher("DummyPairId").eraseToAnyPublisher()
+            }
+            
+            func saveMyId(_ id: String) {}
+            
+            func savePairId(_ id: String) {}
+            
+            func getGlobalFont() -> String {
+                return "font"
+            }
+        }
+        
+        self.dummyRepository = DummyRepository()
+        if let repository = self.dummyRepository {
+            self.getMyIdUseCase = GetMyIdUseCase(userRepository: repository)
+        }
+        
+        guard let usecase = self.getMyIdUseCase else {
+            XCTFail()
+            return
+        }
+        usecase.getMyId().sink { completion in
+            switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        } receiveValue: { id in
+            XCTAssertEqual("00000000-0000-0000-0000-000000000001", id, "전달되는 id 값이 예상 id값과 다릅니다.")
+        }.cancel()
+    }
+    
+}
