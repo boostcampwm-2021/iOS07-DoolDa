753f7dc26a79cd66e9c0adb10de8c9e2a8d06b8b
yabby <yabby1997@gmail.com>
2021-11-03 19:33:12 +0900

    (#36) ✨: 유저가 상대방과 연결될 시 DiaryViewCoordinator를 생성해 start하는 기능 구현

---
+++ Doolda/Doolda/Presentation/Interfaces/PairingViewCoordinatorDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  PairingViewCoordinatorDelegate.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/03.
+//
+
+import Foundation
+
+protocol PairingViewCoordinatorDelegate {
+    func userDidPaired(myId: String, pairId: String)
+}
