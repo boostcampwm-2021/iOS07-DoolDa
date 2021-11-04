2677ff31b4e61a08ac915c2588bebf58cf02db8c
ehWld <729psw@gmail.com>
2021-11-04 18:03:22 +0900

    (#41, #46, #49) ♻️: 코딩 컨벤션에 맞게 수정
    self 명시, internal변수를 먼저 선언

--- Doolda/Doolda/Presentation/ViewModels/SplashViewModel.swift
+++ Doolda/Doolda/Presentation/ViewModels/SplashViewModel.swift
@@ -9,19 +9,22 @@ import Combine
 import Foundation
 
 final class SplashViewModel {
-    @Published var error: Error?
 
-    private var cancellables: Set<AnyCancellable> = []
+    @Published var error: Error?
 
     private let coordinatorDelegate: SplashViewCoordinatorDelegate
     private let getMyIdUseCase: GetMyIdUseCaseProtocol
     private let getPairIdUseCase: GetPairIdUseCaseProtocol
     private let generateMyIdUseCase: GenerateMyIdUseCaseProtocol
+
+    private var cancellables: Set<AnyCancellable> = []
     
-    init(coordinatorDelegate: SplashViewCoordinatorDelegate,
+    init(
+        coordinatorDelegate: SplashViewCoordinatorDelegate,
          getMyIdUseCase: GetMyIdUseCaseProtocol,
          getPairIdUseCase: GetPairIdUseCaseProtocol,
-         generateMyIdUseCase: GenerateMyIdUseCaseProtocol) {
+         generateMyIdUseCase: GenerateMyIdUseCaseProtocol
+    ) {
         self.coordinatorDelegate = coordinatorDelegate
         self.getMyIdUseCase = getMyIdUseCase
         self.getPairIdUseCase = getPairIdUseCase
@@ -29,49 +32,49 @@ final class SplashViewModel {
     }
 
     func prepareUserInfo() {
-        getMyId()
+        self.getMyId()
     }
 
     private func getMyId() {
-        getMyIdUseCase.getMyId()
+        self.getMyIdUseCase.getMyId()
             .sink { [weak self] result in
                 guard case .failure = result else { return }
                 self?.generateMyId()
-            } receiveValue: { myId in
-                self.getPairId(with: myId)
+            } receiveValue: { [weak self] myId in
+                self?.getPairId(with: myId)
             }
-            .store(in: &cancellables)
+            .store(in: &self.cancellables)
     }
 
     private func getPairId(with myId: String) {
-        getPairIdUseCase.getPairId(for: myId)
+        self.getPairIdUseCase.getPairId(for: myId)
             .sink { [weak self] result in
                 guard case .failure(let error) = result else { return }
                 self?.error = error
-            } receiveValue: { pairId in
+            } receiveValue: { [weak self] pairId in
                 if pairId.isEmpty {
-                    self.coordinatorDelegate.userNotPaired(myId: myId)
+                    self?.coordinatorDelegate.userNotPaired(myId: myId)
                 }
-                self.coordinatorDelegate.userAlreadyPaired(myId: myId, pairId: pairId)
+                self?.coordinatorDelegate.userAlreadyPaired(myId: myId, pairId: pairId)
             }
-            .store(in: &cancellables)
+            .store(in: &self.cancellables)
     }
 
     private func generateMyId() {
-        generateMyIdUseCase.savedIdPublisher
-            .sink(receiveValue: { myId in
+        self.generateMyIdUseCase.savedIdPublisher
+            .sink(receiveValue: { [weak self] myId in
                 guard let myId = myId else { return }
-                self.coordinatorDelegate.userNotPaired(myId: myId)
+                self?.coordinatorDelegate.userNotPaired(myId: myId)
             })
-            .store(in: &cancellables)
+            .store(in: &self.cancellables)
 
-        generateMyIdUseCase.errorPublisher
-            .sink(receiveValue: { error in
+        self.generateMyIdUseCase.errorPublisher
+            .sink(receiveValue: { [weak self] error in
                 guard let error = error else { return }
-                self.error = error
+                self?.error = error
             })
-            .store(in: &cancellables)
+            .store(in: &self.cancellables)
 
-        generateMyIdUseCase.generate()
+        self.generateMyIdUseCase.generate()
     }
 }
