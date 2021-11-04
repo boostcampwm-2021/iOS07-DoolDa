510f8b55d07f6dce43ccfb6b0b9ca11732e831e7
Jung Ji Seung <rlsmd9@naver.com>
2021-11-04 19:12:11 +0900

    (#10) ✨: PageEntity 구현

---
+++ Doolda/Doolda/Domain/Entities/PageEntity.swift
@@ -0,0 +1,15 @@
+//
+//  PageEntity.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/04.
+//
+
+import Foundation
+
+struct PageEntity {
+    let pairID: UUID
+    let authorID: UUID
+    let timeStamp: Date
+    let jsonPath: String
+}
