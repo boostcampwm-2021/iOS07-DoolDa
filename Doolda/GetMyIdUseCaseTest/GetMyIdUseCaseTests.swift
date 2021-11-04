872bcc283bf62d3d00fe1800de2a64a4b01aa858
yabby <yabby1997@gmail.com>
2021-11-04 14:53:03 +0900

    (#73) ♻️: 변경된 UserRepository 프로토콜에 맞춰 getMyUseCaseTest 리팩터링, expectation 도입

--- Doolda/GetMyIdUseCaseTest/GetMyIdUseCaseTests.swift
+++ Doolda/GetMyIdUseCaseTest/GetMyIdUseCaseTests.swift
@@ -5,96 +5,130 @@
 //  Created by 김민주 on 2021/11/02.
 //
 
-import XCTest
 import Combine
+import XCTest
 
 class GetMyIdUseCaseTests: XCTestCase {
     private var dummyRepository: UserRepositoryProtocol?
     private var getMyIdUseCase: GetMyIdUseCase?
+    private var cancellables: Set<AnyCancellable> = []
 
     override func tearDownWithError() throws {
         self.getMyIdUseCase = nil
         self.dummyRepository = nil
+        self.cancellables = []
     }
 
     func testGetMyIdSuceess() throws {
-        
         class DummyRepository: UserRepositoryProtocol {
+            enum Errors: Error {
+                case notImplemented
+            }
+
             func fetchMyId() -> AnyPublisher<String, Error> {
-                return Result.Publisher("00000000-0000-0000-0000-000000000001").eraseToAnyPublisher()
+                return Just("00000000-0000-0000-0000-000000000001").setFailureType(to: Error.self).eraseToAnyPublisher()
             }
-            
-            func fetchPairId() -> AnyPublisher<String, Error> {
-                return Result.Publisher("DummyPairId").eraseToAnyPublisher()
+
+            func fetchPairId(for id: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
             }
-            
-            func saveMyId(_ id: String) {}
-            
-            func savePairId(_ id: String) {}
+
+            func saveMyId(_ id: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
+
+            func savePairId(myId: String, friendId: String, pairId: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
+
+            func checkUserIdIsExist(_ id: String) -> AnyPublisher<Bool, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
         }
+        
+        let expectation = self.expectation(description: "testGetMyIdSuceess")
+        var error: Error?
+        var result: String?
         
         self.dummyRepository = DummyRepository()
         if let repository = self.dummyRepository {
             self.getMyIdUseCase = GetMyIdUseCase(userRepository: repository)
         }
-        
-        guard let usecase = self.getMyIdUseCase else {
-            XCTFail()
-            return
-        }
-        usecase.getMyId().sink { completion in
-            switch completion {
-            case .finished:
-                break
-            case .failure(let error):
-                XCTFail(error.localizedDescription)
+
+        guard let usecase = self.getMyIdUseCase else { return XCTFail() }
+
+        usecase.getMyId()
+            .sink { completion in
+                guard case .failure(let encounteredError) = completion else { return }
+                error = encounteredError
+                expectation.fulfill()
+            } receiveValue: { id in
+                result = id
+                expectation.fulfill()
             }
-        } receiveValue: { id in
-            XCTAssertEqual("00000000-0000-0000-0000-000000000001", id, "전달되는 id 값이 예상 id값과 다릅니다.")
-        }.cancel()
+            .store(in: &cancellables)
+        
+        waitForExpectations(timeout: 5)
+        
+        XCTAssertNil(error)
+        XCTAssertEqual("00000000-0000-0000-0000-000000000001", result, "전달되는 id 값이 예상 id값과 다릅니다.")
     }
-    
+
     func testGetMyIdError() throws {
         enum DummyError: Error {
             case dummyError
         }
-        
+
         class DummyRepository: UserRepositoryProtocol {
+            enum Errors: Error {
+                case notImplemented
+            }
+
             func fetchMyId() -> AnyPublisher<String, Error> {
-                return Result.Publisher(DummyError.dummyError).eraseToAnyPublisher()
+                return Fail(error: DummyError.dummyError).eraseToAnyPublisher()
             }
-            
-            func fetchPairId() -> AnyPublisher<String, Error> {
-                return Result.Publisher("DummyPairId").eraseToAnyPublisher()
+
+            func fetchPairId(for id: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
             }
-            
-            func saveMyId(_ id: String) {}
-            
-            func savePairId(_ id: String) {}
+
+            func saveMyId(_ id: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
+
+            func savePairId(myId: String, friendId: String, pairId: String) -> AnyPublisher<String, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
+
+            func checkUserIdIsExist(_ id: String) -> AnyPublisher<Bool, Error> {
+                return Fail(error: Errors.notImplemented).eraseToAnyPublisher()
+            }
         }
         
+        let expectation = self.expectation(description: "testGetMyIdError")
+        var error: DummyError?
+        var result: String?
+
         self.dummyRepository = DummyRepository()
         if let repository = self.dummyRepository {
             self.getMyIdUseCase = GetMyIdUseCase(userRepository: repository)
         }
-        
-        guard let usecase = self.getMyIdUseCase else {
-            XCTFail()
-            return
-        }
-        usecase.getMyId().sink { completion in
-            switch completion {
-            case .finished:
-                break
-            case .failure(let error):
-                guard let dummyError = error as? DummyError else {
-                    XCTFail("전달되는 error 값이 예상 error값과 다릅니다.")
-                    return
-                }
-                XCTAssertEqual(DummyError.dummyError, dummyError, "전달되는 error 값이 예상 error값과 다릅니다.")
+
+        guard let usecase = self.getMyIdUseCase else { return XCTFail() }
+
+        usecase.getMyId()
+            .sink { completion in
+                guard case .failure(let encounteredError) = completion else { return }
+                error = encounteredError as? DummyError
+                expectation.fulfill()
+            } receiveValue: { id in
+                result = id
             }
-        } receiveValue: { id in
-            XCTFail()
-        }.cancel()
+            .store(in: &cancellables)
+        
+        waitForExpectations(timeout: 5)
+        
+        XCTAssertEqual(error, DummyError.dummyError)
+        XCTAssertNil(result)
     }
 }
