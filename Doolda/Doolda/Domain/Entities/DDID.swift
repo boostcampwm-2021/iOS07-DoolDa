64c571298cb9cb521efee2bd2b4d5c5ff9a1e7e7
Jung Ji Seung <rlsmd9@naver.com>
2021-11-05 22:19:56 +0900

    (#85) ✨: DDID Entity 구현

---
+++ Doolda/Doolda/Domain/Entities/DDID.swift
@@ -0,0 +1,26 @@
+//
+//  DDID.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/05.
+//
+
+import Foundation
+
+struct DDID {
+    let id: String
+    
+    init() {
+        self.id = UUID().uuidString
+    }
+    
+    init?(from dto: DDIDDataTransferObject) {
+        guard let id = dto.id,
+              Self.isValid(id: id) else { return nil }
+        self.id = id
+    }
+    
+    static func isValid(id: String) -> Bool {
+        return id.range(of: "\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}", options: .regularExpression) != nil
+    }
+}
