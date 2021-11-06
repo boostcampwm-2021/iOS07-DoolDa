7d7e1eca654eecc52d101b93da5c9d8f482968f8
Jung Ji Seung <rlsmd9@naver.com>
2021-11-06 18:54:51 +0900

    (#86) ♻️: PairUserUseCase 구현

--- Doolda/Doolda/Domain/UseCases/PairUserUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/PairUserUseCase.swift
@@ -34,3 +34,58 @@ protocol PairUserUseCaseProtocol {
     
     func pair(user: User, friendId: DDID)
 }
+
+final class PairUserUseCase: PairUserUseCaseProtocol {
+    var pairedUserPublisher: Published<User?>.Publisher { self.$pairedUser }
+    var errorPublisher: Published<Error?>.Publisher { self.$error }
+    
+    private let userRepository: _UserRepositoryProtocol
+    
+    private var cancellables: Set<AnyCancellable> = []
+    @Published private var pairedUser: User?
+    @Published private var error: Error?
+    
+    init(userRepository: _UserRepositoryProtocol) {
+        self.userRepository = userRepository
+    }
+    
+    func pair(user: User, friendId: DDID) {
+        guard user.id != friendId else {
+            return self.error = PairUserUseCaseError.myIdAndFriendIdAreTheSame
+        }
+        
+        Publishers.Zip(self.userRepository.fetchUser(user), self.userRepository.fetchUser(friendId))
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                self.error = error
+            } receiveValue: { user, friend in
+                // 의문사항: 1. user나 friend가 nil로 전달되는 경우는 네트워크 에러 케이스 아닌가?
+                //         2. 존재하지 않는 친구 아이디 입력후 fetch 요청하면?
+                //              (1, 2)-> 존재하지 않는 아이디를 전달했을 경우 nil 오는가?
+                guard let user = user,
+                      let friend = friend else {
+                          return self.error = PairUserUseCaseError.notExistUser
+                      }
+                
+                if self.canBePaired(user: user, with: friend) {
+                    self.pairedUser = user
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func canBePaired(user: User, with friend: User) -> Bool {
+        if user.pairId == nil && friend.pairId != nil {
+            self.error = PairUserUseCaseError.friendAlreadyPairedWithAnotherUser
+            return false
+        } else if user.pairId != nil && friend.pairId == nil {
+            self.error = PairUserUseCaseError.userAlreadyPairedWithAnotherUser
+            return false
+        } else if user.pairId != nil && friend.pairId != nil && user.pairId != friend.pairId {
+            self.error = PairUserUseCaseError.userAlreadyPairedWithAnotherUser
+            return false
+        } else {
+            return true
+        }
+    }
+}
