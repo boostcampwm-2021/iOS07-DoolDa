91089395ce0df9d9bccc6d6aebbcd32107ab2d0b
Jung Ji Seung <rlsmd9@naver.com>
2021-11-06 19:30:18 +0900

    (#87) ♻️: RefreshUserUseCase 구현

--- Doolda/Doolda/Domain/UseCases/RefreshUserUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/RefreshUserUseCase.swift
@@ -14,3 +14,29 @@ protocol RefreshUserUseCaseProtocol {
     
     func refresh(for user: User)
 }
+
+final class RefreshUserUseCase: RefreshUserUseCaseProtocol {
+    var refreshedUserPublisher: Published<User?>.Publisher { self.$refreshedUser }
+    var errorPublisher: Published<Error?>.Publisher { self.$error }
+    
+    private let userRepository: _UserRepositoryProtocol
+    
+    private var cancellables: Set<AnyCancellable> = []
+    @Published private var refreshedUser: User?
+    @Published private var error: Error?
+    
+    init(userRepository: _UserRepositoryProtocol) {
+        self.userRepository = userRepository
+    }
+    
+    func refresh(for user: User) {
+        self.userRepository.fetchUser(user)
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                self.error = error
+            } receiveValue: { user in
+                self.refreshedUser = user
+            }
+            .store(in: &cancellables)
+    }
+}
