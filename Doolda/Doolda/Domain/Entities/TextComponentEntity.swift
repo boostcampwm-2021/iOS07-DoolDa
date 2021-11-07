8fca961ac7d3715b202310617cae4a5ccc5c547a
ehWld <729psw@gmail.com>
2021-11-08 02:42:33 +0900

    (#32) ✨: TextComponentEntity 구현

--- Doolda/Doolda/Domain/Entities/TextComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/TextComponentEntity.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import CoreGraphics
+
+struct TextComponentEntity: ComponentEntity {
+    enum Color {
+
+    }
+
+    var origin: CGPoint
+    var size: CGSize
+    var angle: CGFloat
+    var text: String
+    var fontSize: CGFloat
+    var fontColor: Color
+
+    func hitTest(at point: CGPoint) -> Bool {
+        return false
+    }
+
+}
