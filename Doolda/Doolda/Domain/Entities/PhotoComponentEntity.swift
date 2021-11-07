55918cc9404b0332322142aea28293147bbb4cb9
ehWld <729psw@gmail.com>
2021-11-08 02:21:18 +0900

    (#27) ✨: PhotoComponentEntity 구현

--- Doolda/Doolda/Domain/Entities/PhotoComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/PhotoComponentEntity.swift
@@ -9,5 +9,27 @@ import Foundation
 import CoreGraphics
 
 struct PhotoComponentEntity: ComponentEntity {
-    
+    let aspectRatio: CGFloat // width / height
+
+    var origin: CGPoint
+    var size: CGSize
+    var angle: CGFloat {
+        get { return wrappedAngle }
+        set { self.wrappedAngle = newValue }
+    }
+    var imageUrl: URL
+
+    @Angle private var wrappedAngle: CGFloat
+
+    init(origin: CGPoint, size: CGSize, angle: CGFloat, aspectRatio: CGFloat, imageUrl: URL) {
+        self.origin = origin
+        self.size = size
+        self.wrappedAngle = angle
+        self.aspectRatio = aspectRatio
+        self.imageUrl = imageUrl
+    }
+
+    func hitTest(at point: CGPoint) -> Bool {
+        return false
+    }
 }
