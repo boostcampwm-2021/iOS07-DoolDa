0e81f8893153ca002d0a48e7ae0e0410e9f12ec5
ehWld <729psw@gmail.com>
2021-11-08 02:00:39 +0900

    (#15) ✨: RawPageEntity에 append 메소드 추가

--- Doolda/Doolda/Domain/Entities/RawPageEntity.swift
+++ Doolda/Doolda/Domain/Entities/RawPageEntity.swift
@@ -13,4 +13,8 @@ typealias BackgroundType = CGColor
 struct RawPageEntity {
     var components: [ComponentEntity]
     var backgroundType: BackgroundType
+
+    mutating func append(_ component: ComponentEntity) {
+        components.append(component)
+    }
 }
