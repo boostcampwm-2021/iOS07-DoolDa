57b167ca5e152b379234b9bc944148482b474201
ehWld <729psw@gmail.com>
2021-11-08 02:46:45 +0900

    (#37) ✨: PhotoFrameEntity 구현

--- Doolda/Doolda/Domain/Entities/PhotoFrameEntity.swift
+++ Doolda/Doolda/Domain/Entities/PhotoFrameEntity.swift
@@ -2,7 +2,14 @@
 //  PhotoFrameEntity.swift
 //  Doolda
 //
-//  Created by Dozzing on 2021/11/08.
+//  Created by Dozzing on 2021/11/07.
 //
 
 import Foundation
+import CoreGraphics
+
+struct PhotoFrameEntity {
+    var frameUrl: URL
+    var requiredPhotoCount: Int { photoBounds.count }
+    let photoBounds: [CGRect]
+}
