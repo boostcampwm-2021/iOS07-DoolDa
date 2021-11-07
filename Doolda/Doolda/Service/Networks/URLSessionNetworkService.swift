635c6afefce38e50a829f0460132ec7d7ca3ee22
MINJUKIM <alsxm553@gmail.com>
2021-11-07 16:48:08 +0900

    (#92) ♻️: resquest에 의미없는 model 파라미터 제거

--- Doolda/Doolda/Service/Networks/URLSessionNetworkService.swift
+++ Doolda/Doolda/Service/Networks/URLSessionNetworkService.swift
@@ -7,9 +7,9 @@
 
 import Combine
 import Foundation
+import Accelerate
 
 class URLSessionNetworkService {
-    
     enum Errors: LocalizedError {
          case invalidUrl
          
@@ -31,18 +31,22 @@ class URLSessionNetworkService {
         self.decoder = JSONDecoder()
     }
     
-    func request<T: Decodable>(_ urlRequest: URLRequestBuilder, model: T.Type) -> AnyPublisher<T, Error> {
+    func request<T: Decodable>(_ urlRequest: URLRequestBuilder) -> AnyPublisher<T, Error> {
         guard let urlRequset = urlRequest.urlRequest else {
             return Fail(error: Errors.invalidUrl).eraseToAnyPublisher()
         }
         return self.session.dataTaskPublisher(for: urlRequset)
-            .tryMap { element -> Data in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                    httpResponse.statusCode == 200 else {
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                    switch (response as? HTTPURLResponse)?.statusCode {
+                    case .some(404):
+                        throw URLError(.cannotFindHost)
+                    default:
                         throw URLError(.badServerResponse)
                     }
-                return element.data
                 }
+                return data
+            }
             .decode(type: T.self, decoder: decoder)
             .eraseToAnyPublisher()
     }
