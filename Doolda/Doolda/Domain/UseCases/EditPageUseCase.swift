7bfe2e19f77b174a28c42be0e8cbb92dc902c65c
yabby <yabby1997@gmail.com>
2021-11-08 23:30:14 +0900

    (#134) ♻️: ComponentEntity를 구조체에서 class로 변경

--- Doolda/Doolda/Domain/UseCases/EditPageUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/EditPageUseCase.swift
@@ -22,3 +22,63 @@ protocol EditPageUseCaseProtocol {
     func changeBackgroundType(_ backgroundType: BackgroundType)
     func savePage()
 }
+
+class EditPageUseCase: EditPageUseCaseProtocol {
+    var selectedComponentPublisher: Published<ComponentEntity?>.Publisher { self.$selectedComponent }
+    var errorPublisher: Published<Error?>.Publisher
+    
+    private var rawPage: RawPageEntity
+    private let pageRepository: PageRepositoryProtocol
+    private let rawPageRepository: RawPageRepositoryProtocol
+    
+    @Published private var selectedComponent: ComponentEntity?
+    
+    init(pageRepository: PageRepositoryProtocol, rawPageRepository: RawPageRepositoryProtocol) {
+        self.rawPage = RawPageEntity()
+        self.pageRepository = pageRepository
+        self.rawPageRepository = rawPageRepository
+    }
+    
+    func selectComponent(at point: CGPoint) {
+        for component in rawPage.components {
+            if component.hitTest(at: point) {
+                self.selectedComponent = component
+                break
+            }
+        }
+    }
+    
+    func moveComponent(difference: CGPoint) {
+        <#code#>
+    }
+    
+    func transformComponent(difference: CGPoint) {
+        <#code#>
+    }
+    
+    func bringComponentForward() {
+        <#code#>
+    }
+    
+    func sendComponentBackward() {
+        <#code#>
+    }
+    
+    func removeComponent() {
+        <#code#>
+    }
+    
+    func addComponent(_ component: ComponentEntity) {
+        <#code#>
+    }
+    
+    func changeBackgroundType(_ backgroundType: BackgroundType) {
+        <#code#>
+    }
+    
+    func savePage() {
+        <#code#>
+    }
+    
+    
+}
