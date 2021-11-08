b99ac9c8786a703f547062f3c45e3b9cd144c04b
ehWld <729psw@gmail.com>
2021-11-09 03:06:22 +0900

    (#152) ✨: ImageComposeUseCase 이미지 변환 스켈레톤 코드 작성

--- Doolda/Doolda/Domain/UseCases/ImageComposeUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/ImageComposeUseCase.swift
@@ -12,3 +12,22 @@ import Foundation
 protocol ImageComposeUseCaseProtocol {
     func compose(photoFrame: PhotoFrameEntity, images: [CIImage]) -> AnyPublisher<CIImage, Error>
 }
+
+class ImageComposeUseCase: ImageComposeUseCaseProtocol {
+    func compose(photoFrame: PhotoFrameEntity, images: [CIImage]) -> AnyPublisher<CIImage, Error> {
+        return Just(CIImage()).tryMap { $0 }.eraseToAnyPublisher()
+    }
+
+    private func crop(with image: CIImage, by frame: CIImage) -> CIImage {
+        return CIImage()
+    }
+
+    private func resize(with image: CIImage, to size: CGSize) -> CIImage {
+        return CIImage()
+    }
+
+    private func translation(with image: CIImage, to point: CGPoint) -> CIImage {
+        return CIImage()
+    }
+
+}
