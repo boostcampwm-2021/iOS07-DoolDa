aaa4c7ab3664a1853a9555b0d80db33e9b2f8b70
yabby <yabby1997@gmail.com>
2021-11-08 23:30:14 +0900

    (#134) ♻️: ComponentEntity를 구조체에서 class로 변경

--- Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
@@ -10,7 +10,6 @@ import Foundation
 
 class StickerComponentEntity: ComponentEntity {
     var name: String
-    @Angle var angle: CGFloat
 
     init(origin: CGPoint, size: CGSize, angle: CGFloat, aspectRatio: CGFloat, name: String) {
         self.name = name
