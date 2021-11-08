209a5b2b507d7494468b9c46e9de16f3e26d16ff
yabby <yabby1997@gmail.com>
2021-11-08 22:19:25 +0900

    (#134) ♻️: PageRepositoryProtocol의 잘못되어있던 프로토콜 명 및 들여쓰기 수정

--- Doolda/Doolda/Domain/Interfaces/PageRepositoryProtocol.swift
+++ Doolda/Doolda/Domain/Interfaces/PageRepositoryProtocol.swift
@@ -8,7 +8,7 @@
 import Combine
 import Foundation
 
-protocol PageRepository {
-  func savePage(_ page: PageEntity) -> AnyPublisher<PageEntity, Error>
-  func fetchPages(for pair: DDID) -> AnyPublisher<[PageEntity], Error>
+protocol PageRepositoryProtocol {
+    func savePage(_ page: PageEntity) -> AnyPublisher<PageEntity, Error>
+    func fetchPages(for pair: DDID) -> AnyPublisher<[PageEntity], Error>
 }
