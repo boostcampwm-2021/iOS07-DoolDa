3656a835219c5eb42bb5b107e8ee457c16bdb81f
yabby <yabby1997@gmail.com>
2021-11-08 19:26:50 +0900

    (#132) 🤝: PageRepository 프로토콜 정의

---
+++ Doolda/Doolda/Domain/Interfaces/PageRepositoryProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  PageRepositoryProtocol.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/08.
+//
+
+import Combine
+import Foundation
+
+protocol PageRepository {
+  func savePage(_ page: PageEntity) -> AnyPublisher<PageEntity, Error>
+  func fetchPages(for pair: DDID) -> AnyPublisher<[PageEntity], Error>
+}
