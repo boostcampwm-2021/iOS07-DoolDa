8b88a64590bda84d1ea286e4ef7ad19dea28a0bd
yabby <yabby1997@gmail.com>
2021-11-08 22:07:33 +0900

    (#156) ✨: CGColor extension 정의

---
+++ Doolda/Doolda/Support/Extensions/CGColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  CGColor+Extensions.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/08.
+//
+
+import UIKit
+
+extension CGColor {
+    static let dooldaBackground = UIColor.dooldaBackground?.cgColor
+}
