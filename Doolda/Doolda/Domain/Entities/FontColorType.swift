c0c3bb6c93e01ba832b2a6ccd65678d8df0c8818
yabby <yabby1997@gmail.com>
2021-11-09 13:36:31 +0900

    (#173) ✨: FontColorType을 CGColor를 RawValue로 가지는 enum으로 정의

---
+++ Doolda/Doolda/Domain/Entities/FontColorType.swift
@@ -0,0 +1,27 @@
+//
+//  FontColorType.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/09.
+//
+
+import CoreGraphics
+import Foundation
+
+enum FontColorType: RawRepresentable, CaseIterable {
+    typealias RawValue = CGColor
+    
+    case black
+    case dooldaLabel
+    
+    init(rawValue: RawValue) {
+        self = .dooldaLabel
+    }
+    
+    var rawValue: RawValue {
+        switch self {
+        case .black: return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        case .dooldaLabel: return #colorLiteral(red: 0.3490196078, green: 0.3490196078, blue: 0.3490196078, alpha: 1)
+        }
+    }
+}
