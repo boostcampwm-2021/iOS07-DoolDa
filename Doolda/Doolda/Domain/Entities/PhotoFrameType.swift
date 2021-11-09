521e12a72c79bafc5ffc9890ecca98a60be0b25f
yabby <yabby1997@gmail.com>
2021-11-09 15:48:04 +0900

    (#173) ♻️: PhotoFrameType을 enum으로 변경, 내부에 PhotoFrame 구조체를 가지도록 개선

--- Doolda/Doolda/Domain/Entities/PhotoFrameType.swift
+++ Doolda/Doolda/Domain/Entities/PhotoFrameType.swift
@@ -8,16 +8,34 @@
 import CoreImage
 import Foundation
 
-struct PhotoFrameType {
-    let baseImage: CIImage
-    let photoBounds: [CGRect]
-    var requiredPhotoCount: Int { photoBounds.count }
-    
-    init?(baseImage: CIImage?, photoBounds: [CGRect]) {
-        guard let baseImage = baseImage else { return nil }
-        self.baseImage = baseImage
-        self.photoBounds = photoBounds
+enum PhotoFrameType: RawRepresentable, CaseIterable {
+    struct PhotoFrame {
+        let baseImage: CIImage
+        let photoBounds: [CGRect]
+        var requiredPhotoCount: Int { photoBounds.count }
+        
+        init?(baseImage: CIImage?, photoBounds: [CGRect]) {
+            guard let baseImage = baseImage else { return nil }
+            self.baseImage = baseImage
+            self.photoBounds = photoBounds
+        }
+        
+        // FIXME: 프레임들을 여기에 static let으로 선언
+        static let lifeFourCuts = PhotoFrame(baseImage: nil, photoBounds: [.zero])
     }
     
-    static let allCases: [PhotoFrameType] = []
+    typealias RawValue = PhotoFrame?
+    
+    case lifeFourCuts
+    
+    init?(rawValue: RawValue) {
+        self = .lifeFourCuts
+    }
+    
+    // FIXME: PhotoFrame내에 선언된 static let인스턴스를 rawValue로 제공
+    var rawValue: PhotoFrame? {
+        switch self {
+        case .lifeFourCuts: return PhotoFrame.lifeFourCuts
+        }
+    }
 }
