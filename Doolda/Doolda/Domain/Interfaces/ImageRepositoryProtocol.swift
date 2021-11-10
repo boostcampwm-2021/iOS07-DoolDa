08d1547f21b8cecd142eed5c30552a29a7ef78e0
ehWld <729psw@gmail.com>
2021-11-10 16:34:52 +0900

    (#152) ✨: 인자로 fileName을 받도록 수정

--- Doolda/Doolda/Domain/Interfaces/ImageRepositoryProtocol.swift
+++ Doolda/Doolda/Domain/Interfaces/ImageRepositoryProtocol.swift
@@ -9,6 +9,6 @@ import Combine
 import Foundation
 
 protocol ImageRepositoryProtocol {
-    func saveLocal(imageData: Data) -> AnyPublisher<URL, Never>
-    func saveRemote(user: User, imageData: Data) -> AnyPublisher<URL, Error>
+    func saveLocal(imageData: Data, fileName: String) -> AnyPublisher<URL, Never>
+    func saveRemote(user: User, imageData: Data, fileName: String) -> AnyPublisher<URL, Error>
 }
