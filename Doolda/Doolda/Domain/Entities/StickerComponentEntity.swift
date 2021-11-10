7db871adde8514f5b5640a3de27fc1ea1e23c315
yabby <yabby1997@gmail.com>
2021-11-10 14:19:27 +0900

    (#124) ✨: ComponentEntity에 Scale 속성 추가

--- Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
@@ -11,8 +11,8 @@ import Foundation
 class StickerComponentEntity: ComponentEntity {
     var name: String
 
-    init(origin: CGPoint, size: CGSize, angle: CGFloat, aspectRatio: CGFloat, name: String) {
+    init(origin: CGPoint, size: CGSize, scale: CGFloat, angle: CGFloat, aspectRatio: CGFloat, name: String) {
         self.name = name
-        super.init(origin: origin, size: size, angle: angle, aspectRatio: aspectRatio)
+        super.init(origin: origin, size: size, scale: scale, angle: angle, aspectRatio: aspectRatio)
     }
 }
