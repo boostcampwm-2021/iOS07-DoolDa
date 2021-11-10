3078b0ba5657b7ced1ad76defb832d5cf3965c77
ehWld <729psw@gmail.com>
2021-11-10 15:25:13 +0900

    (#182) ✨: save method 구현

--- Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
+++ Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
@@ -10,18 +10,10 @@ import Foundation
 
 class FileManagerPersistenceService: FileManagerPersistenceServiceProtocol {
     func save(data: Data, at documents: FileDocuments, fileName: String) -> AnyPublisher<URL, Never> {
-        let fileManager = FileManager.default
         let fileUrl = documents.rawValue.appendingPathComponent(fileName)
         let fileData = NSData(data: data)
 
         fileData.write(to: fileUrl, atomically: true)
-
-        do {
-            try fileData.write(to: fileUrl, atomically: true)
-        } catch let error {
-            return Fail(error: error).eraseToAnyPublisher()
-        }
-
         return Just(fileUrl).eraseToAnyPublisher()
     }    
 }
