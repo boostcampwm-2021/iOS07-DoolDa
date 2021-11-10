2dacbaa34fb5efb1c07973725670c77f2bf5d558
MINJUKIM <alsxm553@gmail.com>
2021-11-10 18:17:21 +0900

    (#187) ✨: Control image static let 추가

--- Doolda/Doolda/Support/Extensions/UIImage+Extensions.swift
+++ Doolda/Doolda/Support/Extensions/UIImage+Extensions.swift
@@ -10,4 +10,8 @@ import UIKit
 extension UIImage {
     static let refresh = UIImage(systemName: "arrow.clockwise")
     static let hedgehogs = UIImage(named: "hedgehogs")
+    static let controlBackward = UIImage(named: "controlBackward")
+    static let controlForward = UIImage(named: "controlForward")
+    static let controlDelete = UIImage(named: "controlDelete")
+    static let controlTransfer = UIImage(named: "controlTransfer")
 }
