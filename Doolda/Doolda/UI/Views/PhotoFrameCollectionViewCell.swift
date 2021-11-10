9de6892277cb8210ddb34bf102e8eb976c19dfba
Jung Ji Seung <rlsmd9@naver.com>
2021-11-10 17:38:13 +0900

    (#161) 🎨: PhotoFramePicker Cell UI작성

---
+++ Doolda/Doolda/UI/Views/PhotoFrameCollectionViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  PhotoFrameCollectionViewCell.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/10.
+//
+
+import UIKit
+
+import SnapKit
+
+class PhotoFrameCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Static Properties
+    
+    static let photoPickerFrameCellId = "photoPickerFrameCellId"
+    
+    // MARK: - Subviews
+    
+    let frameImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        self.addSubview(self.frameImageView)
+        self.frameImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Public Methods
+    
+    func fill(image url: URL?) {
+        self.frameImageView.image = .hedgehogs
+    }
+}
