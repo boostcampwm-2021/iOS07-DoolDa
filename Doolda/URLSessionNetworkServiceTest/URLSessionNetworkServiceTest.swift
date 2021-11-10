74747bae7c43beee1881cf55a3b6db4c883151a5
ehWld <729psw@gmail.com>
2021-11-11 03:32:39 +0900

    (#182) 🧪: firebase storage에 string data를 저장하는 테스트 함수 구현

--- Doolda/URLSessionNetworkServiceTest/URLSessionNetworkServiceTest.swift
+++ Doolda/URLSessionNetworkServiceTest/URLSessionNetworkServiceTest.swift
@@ -5,6 +5,7 @@
 //  Created by Dozzing on 2021/11/10.
 //
 
+import Combine
 import XCTest
 @testable import Doolda
 
@@ -16,8 +17,39 @@ class URLSessionNetworkServiceTest: XCTestCase {
     }
 
 
-    func testExample() throws {
+    func testCreateStorageFileSuccess() throws {
+        guard let networkService = self.networkService else {
+            XCTFail()
+            return
+        }
 
+        let expectation = XCTestExpectation()
+
+        let pairId = "testPairId"
+        let fileName = "testFileName"
+        let fileContent = "Hello this is Doolda Firebase Storage test"
+        guard let fileData = fileContent.data(using: .utf8) else {
+            XCTFail()
+            return
+        }
+
+        let urlRequest = FirebaseAPIs.createStorageFile(pairId, fileName, fileData)
+        let publisher: AnyPublisher<Data, Error> = networkService.request(urlRequest)
+        let subscriber = publisher
+            .sink { completion in
+                guard case .failure( _) = completion else { return }
+                XCTFail()
+            } receiveValue: { data in
+                guard let dataString = String(data: data, encoding: .utf8) else {
+                    XCTFail()
+                    return
+                }
+                XCTAssertEqual(fileContent, dataString)
+                expectation.fulfill()
+            }
+            
+        wait(for: [expectation], timeout: 10)
+        subscriber.cancel()
     }
 
 }
