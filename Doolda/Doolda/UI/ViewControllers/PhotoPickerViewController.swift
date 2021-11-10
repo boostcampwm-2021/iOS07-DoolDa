520064df0177d2c27732b7da42cfdc010b26f50f
Jung Ji Seung <rlsmd9@naver.com>
2021-11-10 23:39:02 +0900

    (#162) 🎨: PhotoPickerCollectionView UI 작성

--- Doolda/Doolda/UI/ViewControllers/PhotoPickerViewController.swift
+++ Doolda/Doolda/UI/ViewControllers/PhotoPickerViewController.swift
@@ -9,19 +9,17 @@ import UIKit
 
 final class PhotoPickerViewController: UIViewController {
     
-    static let photoPickerCellId = "photoPickerCellId"
-    
     // MARK: - Subviews
     
     private lazy var photoPickerCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .vertical
-        flowLayout.minimumLineSpacing = 0
-        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.minimumLineSpacing = 2
+        flowLayout.minimumInteritemSpacing = 2
         flowLayout.sectionInset = .zero
 
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: Self.photoPickerCellId)
+        collectionView.register(PhotoPickerCollectionViewCell.self, forCellWithReuseIdentifier: PhotoPickerCollectionViewCell.photoPickerCellId)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.dataSource = self
@@ -32,13 +30,6 @@ final class PhotoPickerViewController: UIViewController {
     // MARK: - Public Properties
     
     var selectablePhotoCount: Int = 1
-    
-    // MARK: - Initializers
-//
-//    convenience init(selectablePhotoCount: Int) {
-//        self.init(nibName: nil, bundle: nil)
-//        self.selectablePhotoCount = selectablePhotoCount
-//    }
     
     // MARK: - Lifecycle Methods
     
@@ -63,7 +54,9 @@ extension PhotoPickerViewController: UICollectionViewDelegateFlowLayout, UIColle
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAt indexPath: IndexPath
     ) -> CGSize {
-        let cellWidth = collectionView.bounds.width / 3
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
+        
+        let cellWidth = collectionView.bounds.width / 3 - layout.minimumInteritemSpacing
         return CGSize(width: cellWidth, height: cellWidth)
     }
     
@@ -72,10 +65,12 @@ extension PhotoPickerViewController: UICollectionViewDelegateFlowLayout, UIColle
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Self.photoPickerCellId, for: indexPath)
-        cell.backgroundColor = .red
-        cell.layer.borderColor = UIColor.lightGray.cgColor
-        cell.layer.borderWidth = 1
-        return cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoPickerCollectionViewCell.photoPickerCellId, for: indexPath)
+        
+        if let photoPickerCollectionViewCell = cell as? PhotoPickerCollectionViewCell {
+            return photoPickerCollectionViewCell
+        } else {
+            return cell
+        }
     }
 }
