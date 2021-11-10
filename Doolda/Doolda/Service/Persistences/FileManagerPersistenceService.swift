d333a1533075e926baab2ade2fa65de1e3f52c29
ehWld <729psw@gmail.com>
2021-11-10 15:08:28 +0900

    (#182) ✨: FileDocument로 protocol 수정

--- Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
+++ Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
@@ -9,18 +9,19 @@ import Combine
 import Foundation
 
 class FileManagerPersistenceService: FileManagerPersistenceServiceProtocol {
-    func save(data: Data, at documentsUrl: URL, fileName: String) -> AnyPublisher<Data, Error> {
-        
+    func save(data: Data, at documents: FileDocuments, fileName: String) -> AnyPublisher<URL, Never> {
+        let fileManager = FileManager.default
+        let fileUrl = documents.rawValue.appendingPathComponent(fileName)
+        let fileData = NSData(data: data)
+
+        fileData.write(to: fileUrl, atomically: true)
+
+        do {
+            try fileData.write(to: fileUrl, atomically: true)
+        } catch let error {
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+
+        return Just(fileUrl).eraseToAnyPublisher()
     }    
 }
-
-//class temp {
-//    func test() {
-//        let data: Data = Data()
-//        let t = NSData(data: data)
-//        let object = NSObject()
-//
-//        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//        let url2 = FileManager.default.temporaryDirectory
-//    }
-//}
