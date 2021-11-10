e3a00b279f599ad2d29d09305d5326b3fd5ab549
yabby <yabby1997@gmail.com>
2021-11-11 00:03:17 +0900

    (#188) ✨: 360도 각도를 라디안으로 변환해주는 CGFloat 의 computed property 구현

---
+++ Doolda/Doolda/Support/Extensions/CGFloat+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat+Extensions.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/10.
+//
+
+import CoreGraphics
+
+extension CGFloat {
+    var degreeToRadian: CGFloat {
+        return self * .pi / 180
+    }
+}
