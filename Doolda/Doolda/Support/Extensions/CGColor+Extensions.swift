9418f40a9dcd2f5f60c19aa9cfe492ad797c10e7
yabby <yabby1997@gmail.com>
2021-11-10 23:14:13 +0900

    (#188) ♻️: CGColor+Extensions가 UIKit에 의존적이지 않도록 변경

--- Doolda/Doolda/Support/Extensions/CGColor+Extensions.swift
+++ Doolda/Doolda/Support/Extensions/CGColor+Extensions.swift
@@ -5,8 +5,8 @@
 //  Created by Seunghun Yang on 2021/11/08.
 //
 
-import UIKit
+import CoreGraphics
 
 extension CGColor {
-    static let dooldaBackground = UIColor.dooldaBackground?.cgColor
+    static let dooldaBackground = CGColor(red: 245/255, green: 241/255, blue: 231/255, alpha: 1.0)
 }
