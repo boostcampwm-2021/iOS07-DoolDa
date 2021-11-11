a3aab9c3cd335801fbab0ef8fe9a860ec714500b
yabby <yabby1997@gmail.com>
2021-11-11 13:46:04 +0900

    (#188) ♻️: RawPageEntity 간소화

--- Doolda/Doolda/Domain/Entities/RawPageEntity.swift
+++ Doolda/Doolda/Domain/Entities/RawPageEntity.swift
@@ -11,8 +11,8 @@ import Foundation
 struct RawPageEntity {
     var components: [ComponentEntity]
     var backgroundType: BackgroundType
-    var numberOfComponents: Int {
-        self.components.count
+    var count: Int {
+        components.count
     }
     
     init() {
@@ -24,17 +24,11 @@ struct RawPageEntity {
         self.components.append(component)
     }
     
-    mutating func swapAt(at index: Int, with anotherIndex: Int) {
-        guard (0..<self.numberOfComponents).contains(index),
-              (0..<self.numberOfComponents).contains(anotherIndex) else { return }
-        self.components.swapAt(index, anotherIndex)
-    }
-    
     mutating func remove(at index: Int) {
         self.components.remove(at: index)
     }
     
     func indexOf(component: ComponentEntity) -> Int? {
-        return self.components.firstIndex(where: { $0 == component })
+        return self.components.firstIndex(of: component)
     }
 }
