0a0890ee1563b3a3e1f0200bdee5d35072acc781
ehWld <729psw@gmail.com>
2021-11-11 15:25:35 +0900

    (#149) ✨: ImageUseCase 스켈레톤 코드 작성

--- Doolda/Doolda/Domain/UseCases/ImageUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/ImageUseCase.swift
@@ -13,3 +13,23 @@ protocol ImageUseCaseProtocol {
     func saveLocal(image: CIImage) -> AnyPublisher<URL, Never>
     func saveRemote(for user: User, localUrl: URL) -> AnyPublisher<URL, Error>
 }
+
+class ImageUseCase: ImageUseCaseProtocol {
+    private let imageRepository: ImageRepositoryProtocol
+
+    init(imageRepository: ImageRepositoryProtocol) {
+        self.imageRepository = imageRepository
+    }
+
+    func saveLocal(image: CIImage) -> AnyPublisher<URL, Never> {
+        // image를 data로 변환
+        // fileName을 UUID로 생성
+        return imageRepository.saveLocal(imageData: Data(), fileName: "")
+    }
+
+    func saveRemote(for user: User, localUrl: URL) -> AnyPublisher<URL, Error> {
+        // localUrl에서 data로 변환
+        // UUID로 파일이름 생성 ㅎㅎ
+        return imageRepository.saveRemote(user: User(id: DDID(), pairId: DDID()), imageData: Data(), fileName: "")
+    }
+}
