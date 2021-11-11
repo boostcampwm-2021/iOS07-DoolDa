5970e1eb50abec2a5cfba64f023e867e74ea9953
yabby <yabby1997@gmail.com>
2021-11-11 17:13:00 +0900

    (#143) ♻️: RawPageRepository의 프로토콜 재정의

--- Doolda/Doolda/Domain/Interfaces/RawPageRepositoryProtocol.swift
+++ Doolda/Doolda/Domain/Interfaces/RawPageRepositoryProtocol.swift
@@ -9,6 +9,6 @@ import Combine
 import Foundation
 
 protocol RawPageRepositoryProtocol {
-    func saveRawPage(_ rawPage: RawPageEntity) -> AnyPublisher<RawPageEntity, Error>
-    func fetchRawPage(for path: String) -> AnyPublisher<RawPageEntity, Error>
+    func save(rawPage: RawPageEntity, at folder: String, with name: String) -> AnyPublisher<RawPageEntity, Error>
+    func fetch(at folder: String, with name: String) -> AnyPublisher<RawPageEntity, Error>
 }
