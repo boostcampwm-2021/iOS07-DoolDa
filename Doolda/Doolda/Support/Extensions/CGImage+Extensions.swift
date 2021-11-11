555041a8b2bb022cf6a55f814964c3abba607985
Jung Ji Seung <rlsmd9@naver.com>
2021-11-11 19:40:44 +0900

    (#163) ♻️: PhotoFrameType의 baseImage 타입을 CIImage를 활용하도록 수정

---
+++ Doolda/Doolda/Support/Extensions/CGImage+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  CGImage+Extensions.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/11.
+//
+
+import CoreGraphics
+import UIKit
+
+extension CGImage {
+    static let refresh = #imageLiteral(resourceName: "hedgehogs").cgImage
+    static let hedgehogs = #imageLiteral(resourceName: "hedgehogs").cgImage
+}
