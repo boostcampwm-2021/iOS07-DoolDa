331a5cbc7b1b29221dc3e8841494ff978bb279f6
ehWld <729psw@gmail.com>
2021-11-11 14:20:59 +0900

    (#152) ✨: ImageRepositoryError 타입 정의

--- Doolda/Doolda/Data/Repositories/ImageRepository.swift
+++ Doolda/Doolda/Data/Repositories/ImageRepository.swift
@@ -8,6 +8,17 @@
 import Combine
 import Foundation
 
+enum ImageRepositoryError: LocalizedError {
+    case nilUserPairId
+
+    var errorDescription: String? {
+        switch self {
+        case.nilUserPairId:
+            return "유저의 페어 아이디가 존재하지 않습니다."
+        }
+    }
+}
+
 class ImageRepository: ImageRepositoryProtocol {
     private let fileManagerPersistenceService: FileManagerPersistenceServiceProtocol
     private let urlSessionNetworkService: URLSessionNetworkServiceProtocol
@@ -22,6 +33,10 @@ class ImageRepository: ImageRepositoryProtocol {
     }
 
     func saveRemote(user: User, imageData: Data, fileName: String) -> AnyPublisher<URL, Error> {
+        guard let pairId = user.pairId else {
+            return Fail(error: ).eraseToAnyPublisher()
+        }
+        let urlRequest = FirebaseAPIs.createStorageFile(<#T##String#>, <#T##String#>, <#T##Data#>)
         return Just(URL(string: "")!).tryMap{ $0 }.eraseToAnyPublisher()
     }
 
