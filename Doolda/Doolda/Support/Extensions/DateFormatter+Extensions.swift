57588c164bbbbcf58b0411e59e95d6426690d8bd
yabby <yabby1997@gmail.com>
2021-11-15 02:29:53 +0900

    (#133) ✨: Firestore timestamp 데이터 타입을 위한 DateFormatter정의

--- Doolda/Doolda/Support/Extensions/DateFormatter+Extensions.swift
+++ Doolda/Doolda/Support/Extensions/DateFormatter+Extensions.swift
@@ -13,4 +13,10 @@ extension DateFormatter {
         formatter.dateFormat = "YYMMddHHmmss"
         return formatter
     }()
+    
+    static let firestoreFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        return formatter
+    }()
 }
