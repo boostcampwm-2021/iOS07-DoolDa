472024ccf36ed73b3c7887dc294915daece21efe
yabby <yabby1997@gmail.com>
2021-11-15 02:31:34 +0900

    (#133) ♻️: PageDocument의 createdTime으로 올바른 시간이 사용되도록 변경

--- Doolda/Doolda/Data/DataTransferObjects/PageDocument.swift
+++ Doolda/Doolda/Data/DataTransferObjects/PageDocument.swift
@@ -10,17 +10,18 @@ import Foundation
 struct PageDocument: Codable {
     var authorId: String? { return self.fields["author"]?["stringValue"] }
     var pairId: String? { return self.fields["pairId"]?["stringValue"] }
-    var timestamp: String? { return self.fields["createdTime"]?["timestampValue"] }
+    var timeStamp: String? { return self.fields["createdTime"]?["timestampValue"] }
     var jsonPath: String? { return self.fields["jsonPath"]?["stringValue"] }
     let fields: [String: [String: String]]
     
     init(author: String, createdTime: Date, jsonPath: String, pairId: String) {
+        let formattedString = DateFormatter.firestoreFormatter.string(from: createdTime)
         self.fields = [
             "author": [
                 "stringValue": author
             ],
             "createdTime": [
-                "timestampValue": "2014-10-02T15:01:23Z"
+                "timestampValue": formattedString
             ],
             "jsonPath": [
                 "stringValue": jsonPath
@@ -57,9 +58,10 @@ struct PageDocument: Codable {
               let pairId = self.pairId,
               let authorDDID = DDID(from: authorId),
               let pairDDID = DDID(from: pairId),
-//              let date = Date(),
+              let timeStamp = self.timeStamp,
+              let formattedDate = DateFormatter.firestoreFormatter.date(from: timeStamp),
               let jsonPath = self.jsonPath else { return nil }
-        return PageEntity(author: User(id: authorDDID, pairId: pairDDID), timeStamp: Date(), jsonPath: jsonPath)
+        return PageEntity(author: User(id: authorDDID, pairId: pairDDID), timeStamp: formattedDate, jsonPath: jsonPath)
     }
 }
 
