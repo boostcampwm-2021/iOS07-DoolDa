cd37bf8c52233814de7d916bb05fa71797088727
yabby <yabby1997@gmail.com>
2021-11-14 16:50:54 +0900

    (#144) ✨: Components를 decode할 때 타입 정보에 따라 알맞은 Component로 디코드하는기능 구현

--- Doolda/Doolda/Domain/Entities/RawPageEntity.swift
+++ Doolda/Doolda/Domain/Entities/RawPageEntity.swift
@@ -8,13 +8,50 @@
 import CoreGraphics
 import Foundation
 
+enum ComponentType: String, Codable {
+    case base, photo, sticker, text
+}
+
 struct RawPageEntity: Codable {
     var components: [ComponentEntity]
     var backgroundType: BackgroundType
     
+    enum CodingKeys: String, CodingKey {
+        case components, backgroundType
+    }
+    
+    enum ComponentTypeKey: CodingKey {
+        case type
+    }
+    
+    enum Errors: LocalizedError {
+        case encounteredUnknonwnComponent
+    }
+    
     init() {
         self.components = []
         self.backgroundType = .dooldaBackground
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var componentsContainer = try container.nestedUnkeyedContainer(forKey: CodingKeys.components)
+        var typeKeyContainer = componentsContainer
+        self.backgroundType = try container.decode(BackgroundType.self, forKey: CodingKeys.backgroundType)
+        var components: [ComponentEntity] = []
+        
+        while !(typeKeyContainer.isAtEnd && componentsContainer.isAtEnd) {
+            let componentType = try typeKeyContainer
+                .nestedContainer(keyedBy: ComponentTypeKey.self)
+                .decode(ComponentType.self, forKey: ComponentTypeKey.type)
+            switch componentType {
+            case .photo: components.append(try componentsContainer.decode(PhotoComponentEntity.self))
+            case .sticker: components.append(try componentsContainer.decode(StickerComponentEntity.self))
+            case .text: components.append(try componentsContainer.decode(TextComponentEntity.self))
+            default: throw RawPageEntity.Errors.encounteredUnknonwnComponent
+            }
+        }
+        self.components = components
     }
     
     mutating func append(component: ComponentEntity) {
