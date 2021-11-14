2dd9c88602df1cf9ef36b9d62a9e55effa791094
yabby <yabby1997@gmail.com>
2021-11-15 02:30:59 +0900

    (#133) ♻️: Page저장 시 pairId를 안전하게 사용하도록 개선

--- Doolda/Doolda/Data/Repositories/PageRepository.swift
+++ Doolda/Doolda/Data/Repositories/PageRepository.swift
@@ -8,6 +8,16 @@
 import Combine
 import Foundation
 
+enum PageRepositoryError: LocalizedError {
+    case userNotPaired
+    
+    var errorDescription: String? {
+        switch self {
+        case .userNotPaired: return "페어링 된 유저가 없습니다."
+        }
+    }
+}
+
 class PageRepository: PageRepositoryProtocol {
     let urlSessionNetworkService: URLSessionNetworkServiceProtocol
     
@@ -16,7 +26,8 @@ class PageRepository: PageRepositoryProtocol {
     }
     
     func savePage(_ page: PageEntity) -> AnyPublisher<PageEntity, Error> {
-        let request = FirebaseAPIs.createPageDocument(page.author.id.ddidString, page.timeStamp, page.jsonPath, page.author.pairId!.ddidString)
+        guard let pairId = page.author.pairId?.ddidString else { return Fail(error: PageRepositoryError.userNotPaired).eraseToAnyPublisher() }
+        let request = FirebaseAPIs.createPageDocument(page.author.id.ddidString, page.timeStamp, page.jsonPath, pairId)
         let publisher: AnyPublisher<[String: String], Error> = self.urlSessionNetworkService.request(request)
         
         return publisher
