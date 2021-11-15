b7a394788520160b9bb38231c2dfe0ef493c8879
MINJUKIM <alsxm553@gmail.com>
2021-11-15 21:37:57 +0900

    (#224) ♻️: StickerPackEntity 재정의

--- Doolda/Doolda/Domain/Entities/StickerPackEntity.swift
+++ Doolda/Doolda/Domain/Entities/StickerPackEntity.swift
@@ -5,12 +5,23 @@
 //  Created by Dozzing on 2021/11/07.
 //
 
-import CoreGraphics
 import Foundation
 
 struct StickerPackEntity {
     let name: String
-    let packedImageName: String
-    let stickers: [String]
+    var stickersUrl: [URL]
     var isUnpacked: Bool = false
+    
+    init?(name: String) {
+        self.name = name
+        self.stickersUrl = []
+        for index in 0 ... 15 {
+            guard let stickerUrl = Bundle.main.url(forResource: name + "_\(index)", withExtension: "png") else { return nil }
+            self.stickersUrl.append(stickerUrl)
+        }
+    }
+    
+    static let colorStickerPack = StickerPackEntity(name: "colorSticker")
+    static let buddyStickerPack = StickerPackEntity(name: "buddySticker")
+    
 }
