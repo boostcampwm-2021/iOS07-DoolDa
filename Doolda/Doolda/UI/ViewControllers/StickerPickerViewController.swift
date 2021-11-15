cf13c9d615c8482b86aa6d4ede88fb1f41dfc341
ehWld <729psw@gmail.com>
2021-11-16 00:36:42 +0900

    (#228) 🎨: 상단 타이틀, 닫기 버튼 추가

--- Doolda/Doolda/UI/ViewControllers/StickerPickerViewController.swift
+++ Doolda/Doolda/UI/ViewControllers/StickerPickerViewController.swift
@@ -7,7 +7,39 @@
 
 import UIKit
 
+import SnapKit
+
 class StickerPickerViewController: BottomSheetViewController {
+
+    // MARK: - Subviews
+
+    private lazy var bottomSheetTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Dovemayo", size: 16)
+        label.textColor = .dooldaLabel
+        label.text = "스티커 추가"
+        return label
+    }()
+
+    private lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.sizeToFit()
+        return button
+    }()
+
+    private lazy var topStack: UIStackView = {
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                self.bottomSheetTitle,
+                self.closeButton
+            ]
+        )
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
 
     // MARK: - LifeCycle Methods
 
@@ -21,6 +53,14 @@ class StickerPickerViewController: BottomSheetViewController {
 
     private func configureUI() {
         self.detent = .medium
+        self.body.backgroundColor = .dooldaBackground
+
+        self.body.addSubview(self.topStack)
+        self.topStack.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
     }
 
 }
