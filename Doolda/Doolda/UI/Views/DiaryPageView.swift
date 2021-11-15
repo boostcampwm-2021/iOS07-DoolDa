3f12ac90b610404478cb5ab11ffa4bd056d90f84
yabby <yabby1997@gmail.com>
2021-11-15 23:23:33 +0900

    (#260) ✨: DiaryPageViewCell 추가

---
+++ Doolda/Doolda/UI/Views/DiaryPageView.swift
@@ -0,0 +1,23 @@
+//
+//  DiaryPageView.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/15.
+//
+
+import UIKit
+
+class DiaryPageViewCell: UICollectionViewCell {
+    
+    // MARK: - Static Properties
+    
+    static let cellIdentifier: String = "DiaryPageViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
