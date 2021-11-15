020c4b26589dc37c5b1e0378c93042c3146888d9
MINJUKIM <alsxm553@gmail.com>
2021-11-15 22:12:42 +0900

    (#224) ♻️: StickerComponentEntity에서 Url을 가지도록 변경

--- Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
@@ -9,27 +9,27 @@ import CoreGraphics
 import Foundation
 
 class StickerComponentEntity: ComponentEntity {
-    var name: String
+    var stickerUrl: URL
 
     private enum CodingKeys: String, CodingKey {
-        case name, type
+        case stickerUrl, type
     }
     
-    init(frame: CGRect, scale: CGFloat, angle: CGFloat, aspectRatio: CGFloat, name: String) {
-        self.name = name
+    init(frame: CGRect, scale: CGFloat, angle: CGFloat, aspectRatio: CGFloat, stickerUrl: URL) {
+        self.stickerUrl = stickerUrl
         super.init(frame: frame, scale: scale, angle: angle, aspectRatio: aspectRatio)
     }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
+        self.stickerUrl = try container.decode(URL.self, forKey: .stickerUrl)
         try super.init(from: decoder)
     }
     
     override func encode(to encoder: Encoder) throws {
         try super.encode(to: encoder)
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
+        try container.encode(stickerUrl, forKey: .stickerUrl)
         try container.encode(ComponentType.sticker, forKey: .type)
     }
 }
