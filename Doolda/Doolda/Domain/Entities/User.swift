07889a9e95815aa563b1334b0b83609abe24cfb8
yabby <yabby1997@gmail.com>
2021-11-15 23:02:59 +0900

    (#260) ✨: DiffableDataSource를 위해 DDID, User, PageEntity가 Hashable하도록 구현

--- Doolda/Doolda/Domain/Entities/User.swift
+++ Doolda/Doolda/Domain/Entities/User.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
-struct User: Equatable {
+struct User: Hashable {
     let id: DDID
     var pairId: DDID?
     
     init(id: DDID, pairId: DDID? = nil) {
         self.id = id
         self.pairId = pairId
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(pairId)
     }
 }
