555da09b33a0158b81f77a185d816345a81b74c4
MINJUKIM <alsxm553@gmail.com>
2021-11-16 11:35:31 +0900

    (#263 ) ✨: StickerUseCase구현 및 StickerPickerViewModel 간소화

--- Doolda/Doolda/Presentation/ViewModels/StickerPickerViewModel.swift
+++ Doolda/Doolda/Presentation/ViewModels/StickerPickerViewModel.swift
@@ -9,27 +9,22 @@ import CoreGraphics
 import Foundation
 
 protocol StickerPickerViewModelProtocol {
-    var stickerPack: [StickerPackType] { get }
+    func getStickerPacks() -> [StickerPackType]
     func stickerDidSelect(at indexPath: IndexPath) -> StickerComponentEntity?
 }
 
-
 class StickerPickerViewModel: StickerPickerViewModelProtocol {
-    let stickerPack: [StickerPackType]
+    private let stickerUseCase: StickerUseCase
     
-    init() {
-        self.stickerPack = StickerPackType.allCases
+    init(stickerUseCase: StickerUseCase) {
+        self.stickerUseCase = stickerUseCase
+    }
+    
+    func getStickerPacks() -> [StickerPackType] {
+        return stickerUseCase.stickerPacks
     }
     
     func stickerDidSelect(at indexPath: IndexPath) -> StickerComponentEntity? {
-        guard let selectedStickerPack = self.stickerPack[indexPath.section].rawValue else { return nil }
-        let stickerComponentEntity = StickerComponentEntity(
-            frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 100)),
-            scale: 1.0,
-            angle: 0,
-            aspectRatio: 1,
-            stickerUrl: selectedStickerPack.stickersUrl[indexPath.item]
-        )
-        return stickerComponentEntity
+        return self.stickerUseCase.selectSticker(at: indexPath)
     }
 }
