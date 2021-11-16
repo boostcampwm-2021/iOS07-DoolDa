b4b7a7d5684dd5d3cab0cd62ed82ca01ab308e0a
yabby <yabby1997@gmail.com>
2021-11-16 16:53:40 +0900

    (#260) ✨: PageCollectionView를 위한 Header및 Footer 정의

---
+++ Doolda/Doolda/UI/Views/DiaryCollectionViewHeader.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryCollectionViewHeader.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/16.
+//
+
+import UIKit
+
+class DiaryCollectionViewHeader: UICollectionReusableView {
+    
+    // MARK: - Static Properties
+    
+    static let reusableViewIdentifier = "DiaryCollectionViewHeader"
+    
+}
