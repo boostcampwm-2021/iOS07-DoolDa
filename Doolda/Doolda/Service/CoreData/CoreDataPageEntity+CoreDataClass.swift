d5dfa3abb454164ffb5100b1837015229086411e
Jung Ji Seung <rlsmd9@naver.com>
2021-11-16 22:06:39 +0900

    (#241) ✨: CoreData Model 생성

---
+++ Doolda/Doolda/Service/CoreData/CoreDataPageEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataPageEntity+CoreDataClass.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/16.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataPageEntity)
+public class CoreDataPageEntity: NSManagedObject {
+
+}
