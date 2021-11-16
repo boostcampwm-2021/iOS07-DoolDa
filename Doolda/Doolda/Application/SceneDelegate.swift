1799972d89212ef0ed214231433d0c974b1ca10e
MINJUKIM <alsxm553@gmail.com>
2021-11-16 15:02:21 +0900

    (#270) ✨: systemfont를 customfont로 변경하는 함수 추가

--- Doolda/Doolda/Application/SceneDelegate.swift
+++ Doolda/Doolda/Application/SceneDelegate.swift
@@ -13,7 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+        UIFont.overrideInitialize()
+
         let navigationController = UINavigationController()
         
         self.coordinator = AppCoordinator(presenter: navigationController)
