2ec62e498f85f6f0d62c6337a0702f25b0b082af
ehWld <729psw@gmail.com>
2021-11-16 14:26:13 +0900

    (#232, #234) 🎨: Packed, Unpacked Sticker cell 추가

---
+++ Doolda/Doolda/UI/Views/PackedStickerCell.swift
@@ -0,0 +1,18 @@
+//
+//  PackedStickerCell.swift
+//  Doolda
+//
+//  Created by Dozzing on 2021/11/16.
+//
+
+import UIKit
+
+class PackedStickerCell: UICollectionViewCell {
+
+    // MARK: - Static Properties
+
+    static let identifier = "PackedStickerCell"
+
+    // MARK: - Subviews
+
+}
