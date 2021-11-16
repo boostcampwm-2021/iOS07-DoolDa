a185b0e5c306dde901370155189f5afa9b131c30
MINJUKIM <alsxm553@gmail.com>
2021-11-16 18:17:42 +0900

    (#277) ✨: PushNotificationStateUseCase 구현

---
+++ Doolda/Doolda/Domain/UseCases/PushNotificationStateUseCase.swift
@@ -0,0 +1,29 @@
+//
+//  PushNotificationStateUseCase.swift
+//  Doolda
+//
+//  Created by 김민주 on 2021/11/16.
+//
+
+import Foundation
+
+protocol PushNotificationStateUseCaseProtocol {
+    func getPushNotificationState() -> Bool?
+    func setPushNotificationState(as State: Bool)
+}
+
+final class PushNotificationStateUseCase: PushNotificationStateUseCaseProtocol {
+    private let pushNotificationStateRepository: PushNotificationStateRepositoryProtocol
+        
+    init(pushNotificationStateRepository: PushNotificationStateRepositoryProtocol) {
+        self.pushNotificationStateRepository = pushNotificationStateRepository
+    }
+    
+    func getPushNotificationState() -> Bool? {
+        return self.pushNotificationStateRepository.fetchState()
+    }
+    
+    func setPushNotificationState(as state: Bool) {
+        self.pushNotificationStateRepository.saveState(as: state)
+    }
+}
