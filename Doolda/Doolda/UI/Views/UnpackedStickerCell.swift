217a585d657104c27f114bc0455c8868fe77384b
ehWld <729psw@gmail.com>
2021-11-17 19:07:00 +0900

    (#232) ✨: sticker url로 셀을 configure하는 메소드 추가

--- Doolda/Doolda/UI/Views/UnpackedStickerCell.swift
+++ Doolda/Doolda/UI/Views/UnpackedStickerCell.swift
@@ -37,6 +37,11 @@ class UnpackedStickerCell: UICollectionViewCell {
 
     // MARK: - Helpers
 
+    func configure(with sticker: URL) {
+        guard let image = try? UIImage(data: Data(contentsOf: sticker)) else { return }
+        self.imageView.image = image
+    }
+
     private func configureUI() {
         self.addSubview(self.imageView)
         self.imageView.snp.makeConstraints { make in
