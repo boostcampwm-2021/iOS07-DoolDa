ee9dcda3b3830ec3588883b2d66746bbd5fc95f5
Jung Ji Seung <rlsmd9@naver.com>
2021-11-17 16:44:08 +0900

    (#239) ✨: CheckMyTurnUseCase 구현

---
+++ Doolda/Doolda/Domain/UseCases/CheckMyTurnUseCase.swift
@@ -0,0 +1,42 @@
+//
+//  CheckMyTurnUseCaseProtocol.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/16.
+//
+
+import Combine
+import Foundation
+
+protocol CheckMyTurnUseCaseProtocol {
+    func checkTurn(for user: User) -> AnyPublisher<Bool, Error>
+}
+
+class CheckMyTurnUseCase: CheckMyTurnUseCaseProtocol {
+    private let pairRepository: PairRepositoryProtocol
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(pairRepository: PairRepositoryProtocol) {
+        self.pairRepository = pairRepository
+    }
+    
+    func checkTurn(for user: User) -> AnyPublisher<Bool, Error> {
+        if user.id == user.pairId {
+            return Just(true).setFailureType(to: Error.self).eraseToAnyPublisher()
+        }
+        
+        return Future { [weak self] promise in
+            guard let self = self else { return promise(.success(false)) }
+            self.pairRepository.fetchRecentlyEditedUser(with: user)
+                .sink { completion in
+                    guard case .failure(let error) = completion else { return }
+                    promise(.failure(error))
+                } receiveValue: { recentlyEditedUserId in
+                    promise(.success(user.id != recentlyEditedUserId))
+                }
+                .store(in: &self.cancellables)
+        }
+        .eraseToAnyPublisher()
+    }
+}
