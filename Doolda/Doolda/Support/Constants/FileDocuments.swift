f9b05c36275399106db4c0e097e56b1e6cbf9ec5
Jung Ji Seung <rlsmd9@naver.com>
2021-11-18 02:23:44 +0900

    ♻️: FileDocuments에 캐시 디렉터리 접근을 위한 case 추가

--- Doolda/Doolda/Support/Constants/FileDocuments.swift
+++ Doolda/Doolda/Support/Constants/FileDocuments.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 enum FileDocuments {
-    typealias RawValue = URL
+    typealias RawValue = URL?
 
     case temporary
+    case cache
 
     var rawValue: RawValue {
         switch self {
         case .temporary: return FileManager.default.temporaryDirectory
+        case .cache: return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
         }
     }
 }
