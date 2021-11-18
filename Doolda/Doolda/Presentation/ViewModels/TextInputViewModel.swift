6da8cd78a1b9a737f9c3164dd01b2dd104f15de2
MINJUKIM <alsxm553@gmail.com>
2021-11-18 13:30:54 +0900

    (#303) ✨: TextInputViewModel 구현

--- Doolda/Doolda/Presentation/ViewModels/TextInputViewModel.swift
+++ Doolda/Doolda/Presentation/ViewModels/TextInputViewModel.swift
@@ -5,8 +5,21 @@
 //  Created by 김민주 on 2021/11/17.
 //
 
+import CoreGraphics
 import Foundation
 
+protocol TextInputViewModelProtocol {
+    func inputViewEditingDidEnd(with input: NSAttributedString, size:CGFloat, color: FontColorType) -> TextComponentEntity
+}
+
 class TextInputViewModel {
+    private let textUseCase: TextUseCaseProtocol
     
+    init(textUseCase: TextUseCaseProtocol) {
+        self.textUseCase = textUseCase
+    }
+
+    func inputViewEditingDidEnd(with input: NSAttributedString, size:CGFloat, color: FontColorType) -> TextComponentEntity {
+        return self.textUseCase.getTextComponent()
+    }
 }
