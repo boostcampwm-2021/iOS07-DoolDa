cd80a51eb053f2c977f1ebc7ff220955baa0ff4f
Jung Ji Seung <rlsmd9@naver.com>
2021-11-18 11:11:08 +0900

    (#244) ✨: RawPageEntity 캐싱 구현

--- Doolda/Doolda/Data/Repositories/RawPageRepository.swift
+++ Doolda/Doolda/Data/Repositories/RawPageRepository.swift
@@ -7,13 +7,33 @@
 
 import Combine
 import Foundation
+import OSLog
+
+enum RawPageRepositoryError: LocalizedError {
+    case failedToFetchRawPage
+    
+    var errorDescription: String? {
+        switch self {
+        case .failedToFetchRawPage:
+            return "RawPage 패치에 실패했습니다."
+        }
+    }
+}
 
 class RawPageRepository: RawPageRepositoryProtocol {
     private let networkService: URLSessionNetworkServiceProtocol
+    private let fileManagerPersistenceService: FileManagerPersistenceServiceProtocol
     private let encoder: JSONEncoder
     
-    init(networkService: URLSessionNetworkServiceProtocol, encoder: JSONEncoder = JSONEncoder()) {
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(
+        networkService: URLSessionNetworkServiceProtocol,
+        fileManagerPersistenceService: FileManagerPersistenceServiceProtocol,
+        encoder: JSONEncoder = JSONEncoder()
+    ) {
         self.networkService = networkService
+        self.fileManagerPersistenceService = fileManagerPersistenceService
         self.encoder = encoder
     }
     
@@ -32,8 +52,49 @@ class RawPageRepository: RawPageRepositoryProtocol {
     }
     
     func fetch(at folder: String, with name: String) -> AnyPublisher<RawPageEntity, Error> {
-        let request = FirebaseAPIs.downloadDataFile(folder, name)
-        return self.networkService.request(request)
-            .eraseToAnyPublisher()
+        let fileName = "\(folder)\(name)"
+        
+        if self.fileManagerPersistenceService.fileExist(at: .cache, fileName: fileName) {
+            return self.fileManagerPersistenceService.fetch(at: .cache, fileName: fileName)
+                       .decode(type: RawPageEntity.self, decoder: JSONDecoder())
+                       .eraseToAnyPublisher()
+        } else {
+            return self.fetchRawPageEntityFromServer(at: folder, with: name)
+                .map { [weak self] rawPageEntity in
+                    self?.saveRawPageEntityToCache(rawPageEntity: rawPageEntity, fileName: fileName)
+                    return rawPageEntity
+                }
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    private func fetchRawPageEntityFromServer(at folder: String, with name: String) -> AnyPublisher<RawPageEntity, Error> {
+        return Future { [weak self] promise in
+            guard let self = self else { return promise(.failure(RawPageRepositoryError.failedToFetchRawPage)) }
+            
+            let request = FirebaseAPIs.downloadDataFile(folder, name)
+            let publisher: AnyPublisher<RawPageEntity, Error> = self.networkService.request(request)
+            publisher.sink { completion in
+                guard case .failure(let error) = completion else { return }
+                promise(.failure(error))
+            } receiveValue: { rawPageEntity in
+                promise(.success(rawPageEntity))
+            }
+            .store(in: &self.cancellables)
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    private func saveRawPageEntityToCache(rawPageEntity: RawPageEntity, fileName: String) {
+        guard let data = try? JSONEncoder().encode(rawPageEntity) else {
+            return os_log("RawPageEntity caching failure", type: .fault)
+        }
+        
+        self.fileManagerPersistenceService.save(data: data, at: .cache, fileName: fileName)
+            .sink { completion in
+                guard case .failure = completion else { return }
+                os_log("RawPageEntity caching failure", type: .fault)
+            } receiveValue: { _ in }
+            .store(in: &self.cancellables)
     }
 }
