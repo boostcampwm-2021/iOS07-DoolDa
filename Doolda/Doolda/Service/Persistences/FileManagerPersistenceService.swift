552e20d21d776ba7c1934fbe7a87a0bd740e99c4
Jung Ji Seung <rlsmd9@naver.com>
2021-11-20 17:55:59 +0900

    (#331) ♻️: FileDoucuments를 FileManagementService로 이동

--- Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
+++ Doolda/Doolda/Service/Persistences/FileManagerPersistenceService.swift
@@ -8,6 +8,20 @@
 import Combine
 import Foundation
 
+enum FileDocuments {
+    typealias RawValue = URL?
+
+    case temporary
+    case cache
+
+    var rawValue: RawValue {
+        switch self {
+        case .temporary: return FileManager.default.temporaryDirectory
+        case .cache: return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        }
+    }
+}
+
 enum FileManagerPersistenceServiceError: LocalizedError {
     case failedToSaveFile
     case failedToFetchFile
