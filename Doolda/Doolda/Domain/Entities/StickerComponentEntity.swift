daf5b48357c06de90e85478bde89e0d316b62c40
ehWld <729psw@gmail.com>
2021-11-20 16:05:47 +0900

    (#320) 🚨: StickerComponentEntity에 name 추가

--- Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
+++ Doolda/Doolda/Domain/Entities/StickerComponentEntity.swift
@@ -9,27 +9,30 @@ import CoreGraphics
 import Foundation
 
 class StickerComponentEntity: ComponentEntity {
-    var stickerUrl: URL
+    var stickerName: String
+    var stickerUrl: URL? {
+        Bundle.main.url(forResource: self.stickerName, withExtension: "png")
+    }
 
     private enum CodingKeys: String, CodingKey {
-        case stickerUrl, type
+        case stickerName, type
     }
     
-    init(frame: CGRect, scale: CGFloat, angle: CGFloat, aspectRatio: CGFloat, stickerUrl: URL) {
-        self.stickerUrl = stickerUrl
+    init(frame: CGRect, scale: CGFloat, angle: CGFloat, aspectRatio: CGFloat, stickerName: String) {
+        self.stickerName = stickerName
         super.init(frame: frame, scale: scale, angle: angle, aspectRatio: aspectRatio)
     }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.stickerUrl = try container.decode(URL.self, forKey: .stickerUrl)
+        self.stickerName = try container.decode(String.self, forKey: .stickerName)
         try super.init(from: decoder)
     }
     
     override func encode(to encoder: Encoder) throws {
         try super.encode(to: encoder)
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(stickerUrl, forKey: .stickerUrl)
+        try container.encode(stickerName, forKey: .stickerName)
         try container.encode(ComponentType.sticker, forKey: .type)
     }
 }
