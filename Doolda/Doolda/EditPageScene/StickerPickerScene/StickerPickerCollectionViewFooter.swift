e630ec355e810ad4a76ca8d17e4d15b795f50a8f
ehWld <729psw@gmail.com>
2021-11-21 17:32:38 +0900

    (#321) ✨: StickerPackCollectionViewFooter 구현

---
+++ Doolda/Doolda/EditPageScene/StickerPickerScene/StickerPickerCollectionViewFooter.swift
@@ -0,0 +1,54 @@
+//
+//  StickerPickerCollectionViewFooter.swift
+//  Doolda
+//
+//  Created by Dozzing on 2021/11/21.
+//
+
+import UIKit
+
+import SnapKit
+
+class StickerPickerCollectionViewFooter: UICollectionReusableView {
+
+    // MARK: - Static Properties
+
+    static let identifier = "StickerPickerCollectionViewFooter"
+
+    // MARK: - Subviews
+
+    private lazy var title: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .dooldaLabel
+        return label
+    }()
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configureUI()
+    }
+
+    // MARK: - Helpers
+
+    private func configureUI() {
+        self.addSubview(self.title)
+        self.title.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    // MARK: - Public Methods
+
+    func configureStickerPackTilte(with stickerPackName: String) {
+        self.title.text = stickerPackName
+    }
+
+}
