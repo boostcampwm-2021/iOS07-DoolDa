f381c7795a9c8fe4fd451f94a5307a053a7dacf2
yabby <yabby1997@gmail.com>
2021-11-21 12:45:30 +0900

    (#332) 🤝: FIlterBottomSheetViewControllerDelegate 정의

--- Doolda/Doolda/DiaryScene/FilterOptionScene/FilterOptionBottomSheetViewController.swift
+++ Doolda/Doolda/DiaryScene/FilterOptionScene/FilterOptionBottomSheetViewController.swift
@@ -5,4 +5,42 @@
 //  Created by Seunghun Yang on 2021/11/21.
 //
 
-import Foundation
+import UIKit
+
+protocol FilterOptionBottomSheetViewControllerDelegate: AnyObject {
+    func applyButtonDidTap(
+        _ filterOptionBottomSheetViewController: FilterOptionBottomSheetViewController,
+        authorFilter: DiaryAuthorFilter,
+        orderFilter: DiaryOrderFilter
+    )
+}
+
+class FilterOptionBottomSheetViewController: BottomSheetViewController {
+    
+    // MARK: - Subviews
+    
+    // MARK: - Private Properties
+    
+    private weak var delegate: FilterOptionBottomSheetViewControllerDelegate?
+    
+    // MARK: - Initializers
+    
+    convenience init(delegate: FilterOptionBottomSheetViewControllerDelegate?) {
+        self.init(nibName: nil, bundle: nil)
+        self.delegate = delegate
+    }
+    
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        self.detent = .small
+        self.body.backgroundColor = .dooldaBackground
+    }
+}
