5a2a9193643dd1c56deb2e6da0e5b2248522523e
MINJUKIM <alsxm553@gmail.com>
2021-11-22 18:39:03 +0900

    (#351) ✨: TextComponent 변경 시 textEditViewController가 전달

--- Doolda/Doolda/EditPageScene/TextEditScene/TextEditViewModel.swift
+++ Doolda/Doolda/EditPageScene/TextEditScene/TextEditViewModel.swift
@@ -9,12 +9,13 @@ import CoreGraphics
 import Foundation
 
 protocol TextEditViewModelProtocol {
+    var selectedTextComponent: TextComponentEntity? { get }
     func inputViewEditingDidEnd(input: String, contentSize: CGSize, fontSize:CGFloat, color: FontColorType) -> TextComponentEntity
 }
 
 class TextEditViewModel: TextEditViewModelProtocol {
     private let textUseCase: TextUseCaseProtocol
-    private let selectedTextComponent: TextComponentEntity?
+    let selectedTextComponent: TextComponentEntity?
     
     init(textUseCase: TextUseCaseProtocol, selectedTextComponent: TextComponentEntity?) {
         self.textUseCase = textUseCase
@@ -22,6 +23,16 @@ class TextEditViewModel: TextEditViewModelProtocol {
     }
     
     func inputViewEditingDidEnd(input: String, contentSize: CGSize, fontSize: CGFloat, color: FontColorType) -> TextComponentEntity {
-        return self.textUseCase.getTextComponent(with: input, contentSize: contentSize, fontSize: fontSize, color: color)
+        if let selectedTextComponent = selectedTextComponent {
+            return self.textUseCase.changeTextComponent(
+                from: selectedTextComponent,
+                with: input,
+                contentSize: contentSize,
+                fontSize: fontSize,
+                color: color
+            )
+        } else {
+            return self.textUseCase.getTextComponent(with: input, contentSize: contentSize, fontSize: fontSize, color: color)
+        }
     }
 }
