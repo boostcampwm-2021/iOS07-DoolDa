eea540213538dc9892c1a59f76f788d623c9857c
MINJUKIM <alsxm553@gmail.com>
2021-11-23 00:16:12 +0900

    (#353) ✨: FontColorPicker Page 기능 구현

--- Doolda/Doolda/EditPageScene/TextEditScene/TextEditViewController.swift
+++ Doolda/Doolda/EditPageScene/TextEditScene/TextEditViewController.swift
@@ -30,14 +30,30 @@ class TextEditViewController: UIViewController {
         return textView
     }()
     
+    private lazy var fontColorView: FontColorPickerView = {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 40)
+        let fontColorView = FontColorPickerView(
+            frame: frame,
+            collectionViewDelegate: self,
+            collectionViewDataSource: self
+        )
+        return fontColorView
+    }()
+    
     // MARK: - Private Properties
     
     private var widthRatioFromAbsolute: CGFloat?
     private var heightRatioFromAbsolute: CGFloat?
     
+    private var currentColorIndex: CGFloat = 0.0
+    
     private var cancellables: Set<AnyCancellable> = []
     private var viewModel: TextEditViewModelProtocol?
     private weak var delegate: TextEditViewControllerDelegate?
+    
+    override var inputAccessoryView: UIView? {
+            return self.fontColorView
+    }
     
     // MARK: - Initializers
     
@@ -81,7 +97,7 @@ class TextEditViewController: UIViewController {
         self.view.publisher(for: UITapGestureRecognizer())
             .sink { [weak self] _ in
                 guard let self = self,
-                      let textComponenetEntity = self.viewModel?.inputViewEditingDidEnd (
+                      let textComponenetEntity = self.viewModel?.inputViewEditingDidEnd(
                         input: self.inputTextView.text,
                         contentSize: self.computeSizeToAbsolute(with: self.inputTextView.contentSize),
                         fontSize: 16,
@@ -137,5 +153,65 @@ extension TextEditViewController: UITextViewDelegate {
             make.width.equalTo(newSize.width)
             make.height.equalTo(newSize.height)
         }
+    }
+}
+
+extension TextEditViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+    
+}
+
+extension TextEditViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 30, height: 30)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset = collectionView.frame.width / 2.0 - 15
+        return UIEdgeInsets(top: 5, left: inset, bottom: 5, right: inset)
+        }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView,
+                                   withVelocity velocity: CGPoint,
+                                   targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+            if let collectionView = scrollView as? UICollectionView {
+                
+                let cellWidth: CGFloat = 45
+                
+                var offset = targetContentOffset.pointee
+                let index = round((offset.x + collectionView.contentInset.left) / cellWidth)
+                
+                if index > self.currentColorIndex {
+                    self.currentColorIndex += 1
+                } else if index < self.currentColorIndex {
+                    if self.currentColorIndex != 0 {
+                        self.currentColorIndex -= 1
+                    }
+                }
+                
+                offset = CGPoint(x: self.currentColorIndex * cellWidth - collectionView.contentInset.left, y: 0)
+                
+                targetContentOffset.pointee = offset
+                
+            }
+        }
+}
+
+extension TextEditViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FontColorCollectionViewCell.identifier, for: indexPath) as? FontColorCollectionViewCell
+        else { return UICollectionViewCell() }
+
+        cell.configure(with: .red)
+        return cell
     }
 }
