300cec3c73e8f3ca8358159152c2dd4d81d004a9
yabby <yabby1997@gmail.com>
2021-11-22 15:33:23 +0900

    (#348) ✨: 필터링된 결과가 없을 때 보여줄 백그라운드 뷰 추가

---
+++ Doolda/Doolda/DiaryScene/DiaryBackgroundView.swift
@@ -0,0 +1,96 @@
+//
+//  DiaryBackgroundView.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/11/22.
+//
+
+import UIKit
+
+class DiaryBackgroundView: UIView {
+    
+    // MARK: - Subviews
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .dooldaLabel
+        return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .dooldaSubLabel
+        return label
+    }()
+    
+    private lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.imageView, self.titleLabel, self.subtitleLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    // MARK: - Public Properties
+    
+    var image: UIImage? {
+        get { return self.imageView.image }
+        set { self.imageView.image = newValue }
+    }
+    
+    var title: String? {
+        get { return self.titleLabel.text }
+        set { self.titleLabel.text = newValue }
+    }
+    
+    var subtitle: String? {
+        get { return self.subtitleLabel.text }
+        set { self.subtitleLabel.text = newValue }
+    }
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configureUI()
+    }
+    
+    // MARK: - Override Methods
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.configureFont()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        self.addSubview(self.titleStackView)
+        self.titleStackView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-40)
+        }
+        
+        self.imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(200)
+        }
+    }
+    
+    private func configureFont() {
+        self.titleLabel.font = .systemFont(ofSize: 35)
+        self.subtitleLabel.font = .systemFont(ofSize: 20)
+    }
+}
