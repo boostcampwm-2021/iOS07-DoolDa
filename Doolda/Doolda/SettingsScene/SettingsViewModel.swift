d6dbacefc88ba9195078e58b3b1fb44a861abb0b
ehWld <729psw@gmail.com>
2021-11-22 18:36:51 +0900

    (#279) ✨: 뷰모델 프로토콜 구현

--- Doolda/Doolda/SettingsScene/SettingsViewModel.swift
+++ Doolda/Doolda/SettingsScene/SettingsViewModel.swift
@@ -2,11 +2,48 @@
 //  SettingsViewModel.swift
 //  Doolda
 //
-//  Created by 김민주 on 2021/11/17.
+//  Created by Dozzing on 2021/11/22.
 //
 
+import Combine
 import Foundation
 
-class SettingsViewModel {
-    
+protocol SettingsViewModelInput {
+    func fontTypeDidChanged(_ fontName: String)
+    func pushNotificationDidToggle()
+    func openSourceLicenseDidTap()
+    func privacyPolicyDidTap()
+    func contributorDidTap()
+}
+
+protocol SettingsViewModelOutput {
+    var selectedFontPublisher: Published<String>.Publisher { get }
+}
+
+typealias SettingsViewModelProtocol = SettingsViewModelInput & SettingsViewModelOutput
+
+class SettingsViewModel: SettingsViewModelProtocol {
+    var selectedFontPublisher: Published<String>.Publisher { self.$selectedFont }
+
+    @Published private var selectedFont: String
+
+    func fontTypeDidChanged(_ fontName: String) {
+        <#code#>
+    }
+
+    func pushNotificationDidToggle() {
+        <#code#>
+    }
+
+    func openSourceLicenseDidTap() {
+        <#code#>
+    }
+
+    func privacyPolicyDidTap() {
+        <#code#>
+    }
+
+    func contributorDidTap() {
+        <#code#>
+    }
 }
