5c2073a0c5e07285d44023a7b380afdc02b8bc85
ehWld <729psw@gmail.com>
2021-11-23 15:28:01 +0900

    (#322) 🎨: cell에 스위치 버튼과 타입 추가

--- Doolda/Doolda/SettingsScene/SettingsTableViewCell.swift
+++ Doolda/Doolda/SettingsScene/SettingsTableViewCell.swift
@@ -13,7 +13,7 @@ import SnapKit
 class SettingsTableViewCell: UITableViewCell {
 
     enum Style {
-        case detail, disclosure, switchButton
+        case detail, disclosure, switchControl
     }
 
     // MARK: - Static Properties
@@ -33,8 +33,16 @@ class SettingsTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textColor = .dooldaLabel
         return label
-
     }()
+
+    lazy var switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        //switchControl.tintColor = .dooldaHighlighted
+        switchControl.onTintColor = .dooldaHighlighted
+        return switchControl
+    }()
+
+    private lazy var separator = CALayer()
 
     // MARK: - Public Properties
 
@@ -64,6 +72,11 @@ class SettingsTableViewCell: UITableViewCell {
         super.init(coder: coder)
     }
 
+    override func setNeedsLayout() {
+        super.setNeedsLayout()
+        separator.frame = CGRect(x: 16, y: self.frame.height - 1, width: self.frame.width-32, height: 1)
+    }
+
     // MARK: - Helpers
 
     private func configureUI() {
@@ -85,12 +98,27 @@ class SettingsTableViewCell: UITableViewCell {
             make.trailing.equalTo(self.detailLabel.snp.leading)
         }
 
+        self.contentView.addSubview(self.switchControl)
+        self.switchControl.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.bottom.equalToSuperview().offset(-12)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+
+        separator.backgroundColor = UIColor.dooldaLabel?.withAlphaComponent(0.2).cgColor
+        self.layer.addSublayer(self.separator)
+
         switch self.style {
         case .disclosure:
             self.accessoryType = .disclosureIndicator
             self.detailLabel.isHidden = true
+            self.switchControl.isHidden = true
         case .detail:
             self.detailLabel.isHidden = false
+            self.switchControl.isHidden = true
+        case .switchControl:
+            self.detailLabel.isHidden = true
+            self.switchControl.isHidden = false
         default: return
         }
     }
