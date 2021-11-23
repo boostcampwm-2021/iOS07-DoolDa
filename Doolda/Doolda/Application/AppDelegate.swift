48dade33307d348feddf97cfbc76a5b8f9e8918a
yabby <yabby1997@gmail.com>
2021-11-24 03:13:15 +0900

    (#367) ✨: 유저의 FCMToken을 서버에 저장하는 기능 구현 (임시)

--- Doolda/Doolda/Application/AppDelegate.swift
+++ Doolda/Doolda/Application/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 정지승 on 2021/11/01.
 //
 
+import Combine
 import UIKit
 
 import Firebase
@@ -12,6 +13,9 @@ import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -30,6 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
@@ -38,6 +46,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void
     ) {
+        // FIXME: Background Notification에 대응할 코드가 작성되어야 함.
         print("RECEIVED")
     }
     
@@ -46,6 +55,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
     ) {
+        // FIXME: Foreground Notification에 대응할 코드가 작성되어야 함.
         print("RECEIVED")
     }
 }
@@ -53,6 +63,28 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 extension AppDelegate: MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         guard let token = fcmToken else { return }
-        print("FCM TOKEN : \(token)")
+        
+        // FIXME: 더 좋은 방법 찾아보기
+        let networkService = URLSessionNetworkService()
+        let persistenceService = UserDefaultsPersistenceService()
+        let userRepository: UserRepository = UserRepository(persistenceService: persistenceService, networkService: networkService)
+        let fcmTokenRepository: FCMTokenRepository = FCMTokenRepository(urlSessionNetworkService: networkService)
+        let getMyIdUseCase: GetMyIdUseCase = GetMyIdUseCase(userRepository: userRepository)
+        let fcmTokenUseCase: FCMTokenUseCase = FCMTokenUseCase(fcmTokenRepository: fcmTokenRepository)
+        
+        getMyIdUseCase.getMyId()
+            .compactMap { $0 }
+            .sink { [weak self] myId in
+                guard let self = self else { return }
+                fcmTokenUseCase.setToken(for: myId, with: token)
+                    .sink { completion in
+                        guard case .failure(let error) = completion else { return }
+                        print(error.localizedDescription)
+                    } receiveValue: { token in
+                        print("SUCCESS: \(token)")
+                    }
+                    .store(in: &self.cancellables)
+            }
+            .store(in: &self.cancellables)
     }
 }
