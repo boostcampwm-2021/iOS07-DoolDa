fb394fbad53b6242b7d758720fdd1ec6399898a8
ehWld <729psw@gmail.com>
2021-11-23 22:49:40 +0900

    (#326) 🤪: 기능 확인을 위한 임시 폰트 추가

--- Doolda/Doolda/Support/Constants/DoolDaFont.swift
+++ Doolda/Doolda/Support/Constants/DoolDaFont.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 enum DoolDaFont: String, CaseIterable {
-    case dovemayo = "Dovemayo"
-    case test1 = "테스트1"
-    case test2 = "테스트2"
-    case test3 = "테스트3"
-    case test4 = "테스트4"
-    case test5 = "테스트5"
-    case test6 = "테스트6"
+    case dovemayo = "dovemayo"
+    case darae = "darae"
+    case kotraHope = "kotraHope"
+    case uhBeeMysen = "uhBeeMysen"
+    case dungGeunMo = "dungGeunMo"
+    case kyoboHandwriting = "kyoboHandwriting"
+    case appleNeo = "Apple SD Gothic Neo"
 }
