c0fea30d90aebe0bd1182822b0c878afd00221f1
Jung Ji Seung <rlsmd9@naver.com>
2021-11-23 23:29:27 +0900

    (#371) ♻️: CoreDataModel에 updatedTime추가

--- Doolda/Doolda/Service/CoreData/CoreDataPageEntity+CoreDataProperties.swift
+++ Doolda/Doolda/Service/CoreData/CoreDataPageEntity+CoreDataProperties.swift
@@ -17,22 +17,30 @@ extension CoreDataPageEntity {
     @NSManaged public var id: String?
     @NSManaged public var pairId: String?
     @NSManaged public var jsonPath: String?
-    @NSManaged public var timeStamp: Date?
+    @NSManaged public var createdTime: Date?
+    @NSManaged public var updatedTime: Date?
 
     func toPageEntity() -> PageEntity? {
         guard let id = DDID(from: id ?? ""),
               let pairId = DDID(from: pairId ?? ""),
-              let timeStamp = timeStamp,
+              let createdTime = createdTime,
+              let updatedTime = updatedTime,
               let jsonPath = jsonPath else { return nil }
         
-        return PageEntity(author: User(id: id, pairId: pairId), createdTime: timeStamp, jsonPath: jsonPath)
+        return PageEntity(
+            author: User(id: id, pairId: pairId),
+            createdTime: createdTime,
+            updatedTime: updatedTime,
+            jsonPath: jsonPath
+        )
     }
     
     func update(_ pageEntity: PageEntity) {
         self.id = pageEntity.author.id.ddidString
         self.pairId = pageEntity.author.pairId?.ddidString
         self.jsonPath = pageEntity.jsonPath
-        self.timeStamp = pageEntity.createdTime
+        self.createdTime = pageEntity.createdTime
+        self.updatedTime = pageEntity.updatedTime
     }
 }
 
