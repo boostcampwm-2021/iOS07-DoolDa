a2cc50810a4cf544a87144fb149c609f84d49b6a
Jung Ji Seung <rlsmd9@naver.com>
2021-11-25 03:43:22 +0900

    🚨: Page를 저장, 수정시 캐시가 완전히 끝난 후 결과를 반환하도록 수정

--- Doolda/Doolda/Data/Repositories/PageRepository.swift
+++ Doolda/Doolda/Data/Repositories/PageRepository.swift
@@ -12,11 +12,13 @@ import OSLog
 enum PageRepositoryError: LocalizedError {
     case userNotPaired
     case failedToFetchPages
+    case failedToUpdatePage
     
     var errorDescription: String? {
         switch self {
         case .userNotPaired: return "페어링 된 유저가 없습니다."
         case .failedToFetchPages: return "페이지 패치에 실패했습니다."
+        case .failedToUpdatePage: return "페이지 업데이트에 실패했습니다."
         }
     }
 }
@@ -46,25 +48,68 @@ class PageRepository: PageRepositoryProtocol {
     }
     
     func updatePage(_ page: PageEntity) -> AnyPublisher<PageEntity, Error> {
-        guard let pairId = page.author.pairId?.ddidString else { return Fail(error: PageRepositoryError.userNotPaired).eraseToAnyPublisher() }
-        let request = FirebaseAPIs.patchPageDocument(page.author.id.ddidString, page.createdTime, page.updatedTime, page.jsonPath, pairId)
-        let publisher: AnyPublisher<[String: Any], Error> = self.urlSessionNetworkService.request(request)
-        
-        return publisher
-            .map { [weak self] _ -> PageEntity in
-                self?.savePageToCache(pages: [page])
-                return page
+        return Future { [weak self] promise in
+            guard let pairId = page.author.pairId?.ddidString else {
+                return promise(.failure(PageRepositoryError.userNotPaired))
             }
-            .eraseToAnyPublisher()
+            
+            guard let self = self else {
+                return promise(.failure(PageRepositoryError.failedToUpdatePage))
+            }
+            
+            let request = FirebaseAPIs.patchPageDocument(page.author.id.ddidString, page.createdTime, page.updatedTime, page.jsonPath, pairId)
+            let publisher: AnyPublisher<[String: Any], Error> = self.urlSessionNetworkService.request(request)
+            
+            publisher
+                .sink { completion in
+                    guard case .failure(let error) = completion else { return }
+                    promise(.failure(error))
+                } receiveValue: { [weak self] _ in
+                    guard let self = self else {
+                        return promise(.failure(PageRepositoryError.failedToUpdatePage))
+                    }
+                    
+                    self.savePageToCache(pages: [page])
+                        .sink { completion in
+                            guard case .failure(let error) = completion else { return }
+                            promise(.failure(error))
+                        } receiveValue: { _ in
+                            promise(.success(page))
+                        }
+                        .store(in: &self.cancellables)
+                }
+                .store(in: &self.cancellables)
+        }
+        .eraseToAnyPublisher()
     }
     
     func fetchPages(for pair: DDID) -> AnyPublisher<[PageEntity], Error> {
-        return self.fetchPageFromServer(pairId: pair, after: nil)
-            .map { pages -> [PageEntity] in
-                self.savePageToCache(pages: pages)
-                return pages
+        return Future { [weak self] promise in
+            guard let self = self else {
+                return promise(.failure(PageRepositoryError.failedToFetchPages))
             }
-            .eraseToAnyPublisher()
+
+            self.fetchPageFromServer(pairId: pair, after: nil)
+                .sink(receiveCompletion: { completion in
+                    guard case .failure(let error) = completion else { return }
+                    promise(.failure(error))
+                }, receiveValue: { [weak self] pages in
+                    guard let self = self else {
+                        return promise(.failure(PageRepositoryError.failedToFetchPages))
+                    }
+
+                    self.savePageToCache(pages: pages)
+                        .sink(receiveCompletion: { completion in
+                            guard case .failure(let error) = completion else { return }
+                            promise(.failure(error))
+                        }, receiveValue: { _ in
+                            promise(.success(pages))
+                        })
+                        .store(in: &self.cancellables)
+                })
+                .store(in: &self.cancellables)
+        }
+        .eraseToAnyPublisher()
     }
     
     private func fetchPageFromServer(pairId: DDID, after: Date?) -> AnyPublisher<[PageEntity], Error> {
@@ -85,15 +130,12 @@ class PageRepository: PageRepositoryProtocol {
             .eraseToAnyPublisher()
     }
     
-    private func savePageToCache(pages: [PageEntity]) {
+    private func savePageToCache(pages: [PageEntity]) -> AnyPublisher<Void, Error> {
         let savePublishers = pages.map { self.pageEntityPersistenceService.savePageEntity($0) }
         
-        Publishers.MergeMany(savePublishers)
+        return Publishers.MergeMany(savePublishers)
             .collect()
-            .sink { completion in
-                guard case .failure = completion else { return }
-                os_log("PageEntity caching failure", type: .fault)
-            } receiveValue: { _ in }
-            .store(in: &self.cancellables)
+            .map { _ -> Void in () }
+            .eraseToAnyPublisher()
     }
 }
