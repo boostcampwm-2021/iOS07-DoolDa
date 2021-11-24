4dd11ae4d7dc58676d83ee98d1999bb3a2d7fdb6
Jung Ji Seung <rlsmd9@naver.com>
2021-11-24 17:30:41 +0900

    🧪: patchPageDocument 테스트 추가

--- Doolda/Tests/URLSessionNetworkServiceTest/URLSessionNetworkServiceTest.swift
+++ Doolda/Tests/URLSessionNetworkServiceTest/URLSessionNetworkServiceTest.swift
@@ -56,5 +56,33 @@ class URLSessionNetworkServiceTest: XCTestCase {
             
         wait(for: [expectation], timeout: 30)
     }
+    
+    func testPatchPageDocumentSuccess() throws {
+        guard let networkService = self.networkService,
+              let ddid = DDID(from: "00ACAA37-F0BA-49FB-9280-B42D0F68AD1B") else {
+            XCTFail()
+            return
+        }
 
+        let expectation = XCTestExpectation()
+        let currentDate = Date()
+        let jsonPath = "211123230907"
+        
+        let urlRequest = FirebaseAPIs.patchPageDocument(ddid.ddidString, currentDate, currentDate, jsonPath, ddid.ddidString)
+        let publisher: AnyPublisher<[String: Any], Error> = networkService.request(urlRequest)
+
+        publisher
+            .compactMap { $0 }
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                XCTFail("\(error.localizedDescription)")
+                expectation.fulfill()
+            } receiveValue: { result in
+                expectation.fulfill()
+            }
+            .store(in: &cancellableSet)
+            
+        wait(for: [expectation], timeout: 30)
+    }
+    
 }
