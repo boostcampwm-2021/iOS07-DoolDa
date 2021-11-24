04c84fded84b87cb8c716c4bd7af2d7b06db089e
yabby <yabby1997@gmail.com>
2021-11-24 15:31:09 +0900

    (#377, #378, #379) ✨: 각 Push Notification에 대한 Notification 정의 및 수신시 Publish하는 기능 구현

--- Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
+++ Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
@@ -15,19 +15,24 @@ struct PushMessageEntity {
     static let userPairedWithFriend: PushMessageEntity = PushMessageEntity(
         title: "똑똑! 🙋‍♀️🙋‍♂️",
         body: "누군가가 당신을 친구로 연결했어요!",
-        data: [:]
+        data: ["notification": "userPairedWithFriend"]
     )
     
-    // 이거 탭하는 경우엔 새로고쳐야할듯. 새로고침이 성공할 때에는 안보내야함.
     static let userPostedNewPage: PushMessageEntity = PushMessageEntity(
         title: "띵동! 🔔",
         body: "친구가 다이어리를 작성했어요!\n새 다이어리를 확인해볼까요?",
-        data: [:]
+        data: ["notification": "userRequestedNewPage"]
     )
     
     static let userRequestedNewPage: PushMessageEntity = PushMessageEntity(
         title: "쿡쿡! 🥺👉🏻👉🏻",
         body: "친구가 다이어리 작성을 기다리고있어요.\n새 다이어리를 작성해볼까요?",
-        data: [:]
+        data: ["notification": "userRequestedNewPage"]
     )
+    
+    enum Notifications {
+        static let userPairedWithFriend = Notification.Name("userPairedWithFriend")
+        static let userPostedNewPage = Notification.Name("userPostedNewPage")
+        static let userRequestedNewPage = Notification.Name("userRequestedNewPage")
+    }
 }
