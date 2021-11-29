1d98a8541e442b6790ef9aa57fcfb870fa474b56
Jung Ji Seung <rlsmd9@naver.com>
2021-11-29 17:12:33 +0900

    (#451) ♻️: GetUserUseCaseProtocol 분리

---
+++ Doolda/Doolda/Domain/Interfaces/UseCases/GetUserUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GetUserUseCaseProtocol.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/11/29.
+//
+
+import Combine
+import Foundation
+
+protocol GetUserUseCaseProtocol {
+    func getUser(for id: DDID) -> AnyPublisher<User?, Error>
+}
