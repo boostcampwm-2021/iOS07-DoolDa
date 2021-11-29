8d4b394482d59d7b969b326bc2d7977c9d5fc153
ehWld <729psw@gmail.com>
2021-11-29 18:20:26 +0900

    (#445) ✨: 둘다 버튼에 햅틱 반응 추가

--- Doolda/Doolda/Common/DooldaButton.swift
+++ Doolda/Doolda/Common/DooldaButton.swift
@@ -5,15 +5,52 @@
 //  Created by Seunghun Yang on 2021/11/21.
 //
 
+import Combine
 import UIKit
 
 class DooldaButton: UIButton {
+
+    // Private Properties
+
+    private var cancellables: Set<AnyCancellable> = []
+    private lazy var hapticGenerator: UIImpactFeedbackGenerator = {
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        return generator
+    }()
+
+    // Override Properties
+
     override var isEnabled: Bool {
         didSet { self.alpha = self.isEnabled ? 1.0 : 0.5 }
     }
-    
+
+    // Initializers
+
+    init() {
+        super.init(frame: .zero)
+        self.bindUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.bindUI()
+    }
+
+    // LifeCycle Methods
+
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = self.frame.height / 2
+    }
+
+    // Private Methods
+
+    private func bindUI() {
+        self.publisher(for: .touchUpInside)
+            .sink { [weak self] _ in
+                self?.hapticGenerator.prepare()
+                self?.hapticGenerator.impactOccurred()
+            }
+            .store(in: &self.cancellables)
     }
 }
