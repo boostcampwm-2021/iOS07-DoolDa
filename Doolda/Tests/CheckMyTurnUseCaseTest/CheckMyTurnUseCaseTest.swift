55eaaf541949eaa2cb0c9da5a32d8cc82c4e73b0
Jung Ji Seung <rlsmd9@naver.com>
2021-11-30 18:36:55 +0900

    (#481) 🧪: CheckMyTurnUseCaseTest 테스트 케이스 추가

--- Doolda/Tests/CheckMyTurnUseCaseTest/CheckMyTurnUseCaseTest.swift
+++ Doolda/Tests/CheckMyTurnUseCaseTest/CheckMyTurnUseCaseTest.swift
@@ -5,28 +5,59 @@
 //  Created by 정지승 on 2021/11/30.
 //
 
+import Combine
 import XCTest
 
 class CheckMyTurnUseCaseTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    private var cancellables: Set<AnyCancellable> = []
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.cancellables = []
     }
+    
+    func testCheckMyTurnSuccess() {
+        let checkMyTurnUseCase = CheckMyTurnUseCase(pairRepository: DummyPairRepository(isSuccessMode: true))
+        let user = User(id: DDID(), pairId: nil, friendId: nil)
+        
+        let expectation = expectation(description: #function)
+        var errorResult: Error?
+        
+        checkMyTurnUseCase.checkTurn(for: user)
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                errorResult = error
+                expectation.fulfill()
+            } receiveValue: { isMyTurn in
+                XCTAssertFalse(isMyTurn)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        waitForExpectations(timeout: 10)
+        
+        XCTAssertNil(errorResult)
     }
+    
+    func testCheckMyTurnFailure() {
+        let checkMyTurnUseCase = CheckMyTurnUseCase(pairRepository: DummyPairRepository(isSuccessMode: false))
+        let user = User(id: DDID(), pairId: nil, friendId: nil)
+        
+        let expectation = expectation(description: #function)
+        var errorResult: Error?
+        
+        checkMyTurnUseCase.checkTurn(for: user)
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                errorResult = error
+                expectation.fulfill()
+            } receiveValue: { isMyTurn in
+                XCTAssertFalse(isMyTurn)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        waitForExpectations(timeout: 10)
+        
+        XCTAssertNotNil(errorResult)
     }
-
 }
