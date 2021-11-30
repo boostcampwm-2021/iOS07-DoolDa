4457ac4bfe0b5e2c51123bc234c13153930cac7e
Jung Ji Seung <rlsmd9@naver.com>
2021-11-30 15:31:28 +0900

    (#460) 🧪: 혼자 친구맺기 테스트 추가

---
+++ Doolda/Tests/PairUserUseCaseTest/PairUserUseCaseTest.swift
@@ -0,0 +1,85 @@
+//
+//  PairUserUseCaseTest.swift
+//  PairUserUseCaseTest
+//
+//  Created by 정지승 on 2021/11/30.
+//
+
+import Combine
+import XCTest
+
+class PairUserUseCaseTest: XCTestCase {
+    private var cancellables: Set<AnyCancellable> = []
+
+    override func tearDownWithError() throws {
+        self.cancellables = []
+    }
+
+    func testPairWithMyselfSuccess() {
+        let pairUserUseCase = PairUserUseCase(
+            userRepository: DummyUserRepository(isSuccessMode: true),
+            pairRepository: DummyPairRepository(isSuccessMode: true)
+        )
+        
+        let expectation = expectation(description: #function)
+        
+        let user = User(id: DDID(), pairId: nil, friendId: nil)
+        
+        pairUserUseCase.pair(user: user)
+        pairUserUseCase.pairedUserPublisher
+            .sink { user in
+                if let user = user,
+                   let pairId = user.pairId,
+                   let friendId = user.friendId {
+                    XCTAssertEqual(user.id, friendId)
+                    XCTAssertEqual(user.id, pairId)
+                } else {
+                    XCTFail()
+                }
+                
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        var errorResult: Error?
+        pairUserUseCase.errorPublisher
+            .sink { error in
+                errorResult = error
+            }
+            .store(in: &self.cancellables)
+        
+        waitForExpectations(timeout: 10)
+        
+        XCTAssertNil(errorResult)
+    }
+    
+    func testPairWithMyselfFailure() {
+        let pairUserUseCase = PairUserUseCase(
+            userRepository: DummyUserRepository(isSuccessMode: false),
+            pairRepository: DummyPairRepository(isSuccessMode: false)
+        )
+        
+        let expectation = expectation(description: #function)
+        
+        let user = User(id: DDID(), pairId: nil, friendId: nil)
+        
+        pairUserUseCase.pair(user: user)
+        pairUserUseCase.pairedUserPublisher
+            .sink { user in
+                XCTAssertNil(user?.pairId)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        var errorResult: Error?
+        pairUserUseCase.errorPublisher
+            .sink { error in
+                errorResult = error
+            }
+            .store(in: &self.cancellables)
+        
+        waitForExpectations(timeout: 10)
+        
+        XCTAssertNotNil(errorResult)
+    }
+}
