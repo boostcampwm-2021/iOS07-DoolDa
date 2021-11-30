48a402b408ae376c2d7b5dc73f3616b527031552
yabby <yabby1997@gmail.com>
2021-11-30 14:52:51 +0900

    (#459) 🧪: GetUserUseCase 테스트 및 DummyUserRepository 정의

--- Doolda/Tests/Dummies/DummyUserRepository.swift
+++ Doolda/Tests/Dummies/DummyUserRepository.swift
@@ -1,17 +1,27 @@
 //
 //  DummyUserRepository.swift
-//  GetMyIdUseCaseTest
 //
-//  Created by 김민주 on 2021/11/30.
+//  EditPageUseCaseTest
+//  Created by Seunghun Yang on 2021/11/30.
 //
-
 import Combine
+
 import Foundation
 
 class DummyUserRepository: UserRepositoryProtocol {
     var isSuccessMode: Bool = true
     var dummyMyId: DDID
-
+    
+    static let firstUserId = DDID()
+    static let secondUserId = DDID()
+    static let thirdUserId = DDID()
+    
+    private var userTable: [DDID: User] = [
+        DummyUserRepository.firstUserId: User(id: DummyUserRepository.firstUserId, pairId: nil, friendId: nil),
+        DummyUserRepository.secondUserId: User(id: DummyUserRepository.secondUserId, pairId: nil, friendId: nil),
+        DummyUserRepository.thirdUserId: User(id: DummyUserRepository.thirdUserId, pairId: nil, friendId: nil)
+    ]
+    
     init(dummyMyId: DDID, isSuccessMode: Bool = true) {
         self.dummyMyId = dummyMyId
         self.isSuccessMode = isSuccessMode
@@ -25,15 +35,15 @@ class DummyUserRepository: UserRepositoryProtocol {
         return self.isSuccessMode ? Just(dummyMyId).eraseToAnyPublisher() : Just(nil).eraseToAnyPublisher()
     }
     
-    func fetchUser(_ id: DDID) -> AnyPublisher<User?, Error> {
-        return Fail(error: DummyError.notImplemented).eraseToAnyPublisher()
-    }
-    
     func setUser(_ user: User) -> AnyPublisher<User, Error> {
         return Fail(error: DummyError.notImplemented).eraseToAnyPublisher()
     }
     
     func resetUser(_ user: User) -> AnyPublisher<User, Error> {
+        return Fail(error: DummyError.notImplemented).eraseToAnyPublisher()
+    }
+    
+    func fetchUser(_ id: DDID) -> AnyPublisher<User?, Error> {
         return Fail(error: DummyError.notImplemented).eraseToAnyPublisher()
     }
     
