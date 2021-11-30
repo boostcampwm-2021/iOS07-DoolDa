2a31036ff0d74edf0598bbb8235f82de8df0f7d4
Jung Ji Seung <rlsmd9@naver.com>
2021-11-30 18:10:22 +0900

    (#476) 🧪: RefreshUserUseCaseTest 테스트 케이스 추가

--- Doolda/Tests/RefreshUserUseCaseTest/RefreshUserUseCaseTest.swift
+++ Doolda/Tests/RefreshUserUseCaseTest/RefreshUserUseCaseTest.swift
@@ -5,28 +5,75 @@
 //  Created by 정지승 on 2021/11/30.
 //
 
+import Combine
 import XCTest
 
 class RefreshUserUseCaseTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    private var cancellables: Set<AnyCancellable> = []
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.cancellables = []
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testRefreshUserSuccess() {
+        let refreshUserUseCase = RefreshUserUseCase(
+            userRepository: DummyUserRepository(
+                dummyMyId: DummyUserRepository.fourthUserId,
+                isSuccessMode: true
+            )
+        )
+        
+        let expectation = expectation(description: #function)
+        expectation.expectedFulfillmentCount = 2
+        
+        let user = User(id: DummyUserRepository.fourthUserId, pairId: nil, friendId: nil)
+        refreshUserUseCase.refresh(for: user)
+        refreshUserUseCase.refreshedUserPublisher
+            .sink { user in
+                XCTAssertNotNil(user)
+                XCTAssertNotNil(user?.friendId)
+                XCTAssertNotNil(user?.pairId)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        refreshUserUseCase.errorPublisher
+            .sink { error in
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        waitForExpectations(timeout: 10)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRefreshUserFailure() {
+        let refreshUserUseCase = RefreshUserUseCase(
+            userRepository: DummyUserRepository(
+                dummyMyId: DummyUserRepository.fourthUserId,
+                isSuccessMode: false
+            )
+        )
+        
+        let expectation = expectation(description: #function)
+        expectation.expectedFulfillmentCount = 2
+        
+        let user = User(id: DummyUserRepository.fourthUserId, pairId: nil, friendId: nil)
+        refreshUserUseCase.refresh(for: user)
+        refreshUserUseCase.refreshedUserPublisher
+            .sink { user in
+                XCTAssertNil(user)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        refreshUserUseCase.errorPublisher
+            .sink { error in
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+        
+        waitForExpectations(timeout: 10)
     }
-
 }
