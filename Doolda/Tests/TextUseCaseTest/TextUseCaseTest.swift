4e109867bbde67e79e8f61f529379e6245902995
MINJUKIM <alsxm553@gmail.com>
2021-11-30 18:27:00 +0900

    (#479) 🧪: TextUseCaseTest 추가

---
+++ Doolda/Tests/TextUseCaseTest/TextUseCaseTest.swift
@@ -0,0 +1,25 @@
+//
+//  TextUseCaseTest.swift
+//  TextUseCaseTest
+//
+//  Created by 김민주 on 2021/11/30.
+//
+
+import XCTest
+
+class TextUseCaseTest: XCTestCase {
+
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
