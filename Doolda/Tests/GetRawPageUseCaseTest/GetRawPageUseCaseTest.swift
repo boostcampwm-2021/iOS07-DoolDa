c26e7141366079a819f4ec4941ec7379fcea5f9f
Jung Ji Seung <rlsmd9@naver.com>
2021-11-30 20:32:49 +0900

    (#488) 🧪: GetRawPageUseCase Target 생성

---
+++ Doolda/Tests/GetRawPageUseCaseTest/GetRawPageUseCaseTest.swift
@@ -0,0 +1,32 @@
+//
+//  GetRawPageUseCaseTest.swift
+//  GetRawPageUseCaseTest
+//
+//  Created by 정지승 on 2021/11/30.
+//
+
+import XCTest
+
+class GetRawPageUseCaseTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
