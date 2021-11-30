d3011e47d39164c51e5f847dd8345d78a0bb2a14
MINJUKIM <alsxm553@gmail.com>
2021-11-30 18:27:26 +0900

    ♻️: ComponentType 파일 분리

---
+++ Doolda/Doolda/Domain/Entities/ComponentType.swift
@@ -0,0 +1,12 @@
+//
+//  ComponentType.swift
+//  Doolda
+//
+//  Created by 김민주 on 2021/11/30.
+//
+
+import Foundation
+
+enum ComponentType: String, Codable {
+    case base, photo, sticker, text
+}
