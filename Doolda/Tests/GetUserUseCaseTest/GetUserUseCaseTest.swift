cb59734c287073aad2afd5949c4fd92eabca7154
yabby <yabby1997@gmail.com>
2021-11-30 15:11:56 +0900

    (#459) 🧪: DummyUserRepository 테스트 완료

--- Doolda/Tests/GetUserUseCaseTest/GetUserUseCaseTest.swift
+++ Doolda/Tests/GetUserUseCaseTest/GetUserUseCaseTest.swift
@@ -16,10 +16,56 @@ class GetUserUseCaseTest: XCTestCase {
     }
     
     func testGetUserSuccess() {
+        let userRepository = DummyUserRepository(isSuccessMode: true)
+        let getUserUseCase = GetUserUseCase(userRepository: userRepository)
         
+        let expectation = self.expectation(description: "testGetUserSuccess")
+        
+        var error: Error?
+        var result: User?
+        
+        getUserUseCase.getUser(for: DummyUserRepository.firstUserId)
+            .compactMap { $0 }
+            .sink { completion in
+                guard case .failure(let encounteredError) = completion else { return }
+                error = encounteredError
+                expectation.fulfill()
+            } receiveValue: { user in
+                result = user
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+
+        waitForExpectations(timeout: 1)
+        
+        XCTAssertNil(error)
+        XCTAssertEqual(result, User(id: DummyUserRepository.firstUserId, pairId: nil, friendId: nil))
     }
     
     func testGetUserFailure() {
+        let userRepository = DummyUserRepository(isSuccessMode: false)
+        let getUserUseCase = GetUserUseCase(userRepository: userRepository)
         
+        let expectation = self.expectation(description: "testGetUserFailure")
+        
+        var error: Error?
+        var result: User?
+        
+        getUserUseCase.getUser(for: DummyUserRepository.thirdUserId)
+            .compactMap { $0 }
+            .sink { completion in
+                guard case .failure(let encounteredError) = completion else { return }
+                error = encounteredError
+                expectation.fulfill()
+            } receiveValue: { user in
+                result = user
+                expectation.fulfill()
+            }
+            .store(in: &self.cancellables)
+
+        waitForExpectations(timeout: 1)
+        
+        XCTAssertNotNil(error)
+        XCTAssertNotEqual(result, User(id: DummyUserRepository.thirdUserId, pairId: nil, friendId: nil))
     }
 }
