fe04ed8e9ebf7d209fb0e0b2a92b1f5ef879a4ab
Jung Ji Seung <rlsmd9@naver.com>
2021-12-02 01:02:30 +0900

    ♻️: FilterOptionViewModel의 Published.Publisher를 AnyPublisher로 수정

--- Doolda/Doolda/DiaryScene/FilterOptionScene/FilterOptionBottomSheetViewModel.swift
+++ Doolda/Doolda/DiaryScene/FilterOptionScene/FilterOptionBottomSheetViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Seunghun Yang on 2021/11/21.
 //
 
+import Combine
 import Foundation
 
 protocol FilterOptionBottomSheetViewModelInput {
@@ -13,15 +14,15 @@ protocol FilterOptionBottomSheetViewModelInput {
 }
 
 protocol FilterOptionBottomSheetViewModelOutput {
-    var authorFilterPublisher: Published<DiaryAuthorFilter>.Publisher { get }
-    var orderFilterPublisher: Published<DiaryOrderFilter>.Publisher { get }
+    var authorFilterPublisher: AnyPublisher<DiaryAuthorFilter, Never> { get }
+    var orderFilterPublisher: AnyPublisher<DiaryOrderFilter, Never> { get }
 }
 
 typealias FilterOptionBottomSheetViewModelProtocol = FilterOptionBottomSheetViewModelInput & FilterOptionBottomSheetViewModelOutput
 
 class FilterOptionBottomSheetViewModel: FilterOptionBottomSheetViewModelInput, FilterOptionBottomSheetViewModelOutput {
-    var authorFilterPublisher: Published<DiaryAuthorFilter>.Publisher { self.$authorFilter }
-    var orderFilterPublisher: Published<DiaryOrderFilter>.Publisher { self.$orderFilter }
+    var authorFilterPublisher: AnyPublisher<DiaryAuthorFilter, Never> { self.$authorFilter.eraseToAnyPublisher() }
+    var orderFilterPublisher: AnyPublisher<DiaryOrderFilter, Never> { self.$orderFilter.eraseToAnyPublisher() }
     
     @Published private var authorFilter: DiaryAuthorFilter = .both
     @Published private var orderFilter: DiaryOrderFilter = .descending
