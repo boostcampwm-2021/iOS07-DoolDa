f49dba64b8e5f55055332c3c8b0744f38e2d5408
Jung Ji Seung <rlsmd9@naver.com>
2021-12-01 16:40:20 +0900

    ♻️: PageDetailView 코디네이터 개선

--- Doolda/Doolda/PageDetailScene/PageDetailViewCoordinator.swift
+++ Doolda/Doolda/PageDetailScene/PageDetailViewCoordinator.swift
@@ -5,22 +5,58 @@
 //  Created by Dozzing on 2021/11/24.
 //
 
+import Combine
 import UIKit
 
-final class PageDetailViewCoordinator: PageDetailViewCoordinatorProtocol {
+final class PageDetailViewCoordinator: CoordinatorProtocol {
+    
+    // MARK: - Nested enum
+    
+    enum Notifications {
+        static let editPageRequested = Notification.Name("editPageRequested")
+    }
+    
+    enum Keys {
+        static let rawPageEntity = "rawPageEntity"
+    }
+    
+    // MARK: - Public Properties
+    
     var identifier: UUID
     var presenter: UINavigationController
     var children: [UUID : CoordinatorProtocol] = [:]
+    
+    // MARK: - Private Properties
+    
     private let user: User
     private let pageEntity: PageEntity
+    
+    private var cancellables: Set<AnyCancellable> = []
 
+    // MARK: - Initializers
+    
     init(identifier: UUID, presenter: UINavigationController, user: User, pageEntity: PageEntity) {
         self.identifier = identifier
         self.presenter = presenter
         self.user = user
         self.pageEntity = pageEntity
+        bind()
     }
 
+    // MARK: - Helpers
+    
+    private func bind() {
+        NotificationCenter.default.publisher(for: Notifications.editPageRequested, object: nil)
+            .compactMap { $0.userInfo?[Keys.rawPageEntity] as? RawPageEntity }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] rawPageEntity in
+                self?.editPageRequested(with: rawPageEntity)
+            }
+            .store(in: &self.cancellables)
+    }
+    
+    // MARK: - Public Methods
+    
     func start() {
         let networkService = URLSessionNetworkService.shared
         let coreDataPersistenceService = CoreDataPersistenceService.shared
@@ -38,21 +74,25 @@ final class PageDetailViewCoordinator: PageDetailViewCoordinatorProtocol {
         let viewModel = PageDetaillViewModel(
             user: self.user,
             pageEntity: self.pageEntity,
-            coordinator: self,
             getRawPageUseCase: getRawPageUseCase
         )
 
-        DispatchQueue.main.async {
-            let viewController = PageDetailViewController(viewModel: viewModel)
-            self.presenter.topViewController?.navigationController?.pushViewController(viewController, animated: true)
-        }
+        let viewController = PageDetailViewController(viewModel: viewModel)
+        self.presenter.topViewController?.navigationController?.pushViewController(viewController, animated: true)
     }
 
-    func editPageRequested(with rawPageEntity: RawPageEntity) {
+    // MARK: - Private Methods
+    
+    private func editPageRequested(with rawPageEntity: RawPageEntity) {
         let identifier = UUID()
-        let coordinator = EditPageViewCoordinator(identifier: identifier, presenter: self.presenter, user: self.user, pageEntity: self.pageEntity, rawPageEntity: rawPageEntity)
+        let coordinator = EditPageViewCoordinator(
+            identifier: identifier,
+            presenter: self.presenter,
+            user: self.user,
+            pageEntity: self.pageEntity,
+            rawPageEntity: rawPageEntity
+        )
         self.children[identifier] = coordinator
         coordinator.start()
     }
-
 }
