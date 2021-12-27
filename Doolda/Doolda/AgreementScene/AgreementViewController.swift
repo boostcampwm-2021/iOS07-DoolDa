63710a1e050d60247b23ba55f745a971c08a5129
Jung Ji Seung <rlsmd9@naver.com>
2021-12-27 20:26:34 +0900

    (#501) ✨: AgreementViewController 생성

---
+++ Doolda/Doolda/AgreementScene/AgreementViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AgreementViewController.swift
+//  Doolda
+//
+//  Created by 정지승 on 2021/12/27.
+//
+
+import UIKit
+
+final class AgreementViewController: UIViewController {
+    
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
