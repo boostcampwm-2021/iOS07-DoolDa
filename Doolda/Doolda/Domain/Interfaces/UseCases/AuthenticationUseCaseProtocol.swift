5731fec68a046a36bfe8cb1aaed6946af2359a5a
yabby <yabby1997@gmail.com>
2021-12-27 22:43:56 +0900

    (#503) ✨: FirebaseAuth 패키지 설치 및 AuthenticationUseCaseProtocol정의

---
+++ Doolda/Doolda/Domain/Interfaces/UseCases/AuthenticationUseCaseProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  AuthenticationUseCaseProtocol.swift
+//  Doolda
+//
+//  Created by Seunghun Yang on 2021/12/27.
+//
+
+import AuthenticationServices
+import Combine
+import Foundation
+
+import FirebaseAuth
+
+protocol AuthenticationUseCaseProtocol {
+    func getCurrentUser() -> FirebaseAuth.User?
+    func signIn(credential: AuthCredential, completion: ((AuthDataResult?, Error?) -> Void)?)
+    func signOut() throws
+    func delete() -> AnyPublisher<Void, Error>
+}
