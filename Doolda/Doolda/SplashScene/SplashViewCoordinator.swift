92d500c7af006bbc8751b5d04746b3cce1efbe3b
yabby <yabby1997@gmail.com>
2021-12-28 17:57:32 +0900

    (#512) ✨: 애플 로그인에 따른 유저 검증 로직 변경 및 그에따라 영향받는 부분과 함께 전반적인 코드 리팩터링

--- Doolda/Doolda/SplashScene/SplashViewCoordinator.swift
+++ Doolda/Doolda/SplashScene/SplashViewCoordinator.swift
@@ -10,9 +10,11 @@ import UIKit
 
 final class SplashViewCoordinator: BaseCoordinator {
     
-    //MARK: - Nested enum
+    // MARK: - Nested enum
     
     enum Notifications {
+        static let userNotLoggedIn = Notification.Name("userNotLoggedIn")
+        static let userNotExists = Notification.Name("userNotExists")
         static let userNotPaired = Notification.Name("userNotPaired")
         static let userAlreadyPaired = Notification.Name("userAlreadyPaired")
     }
@@ -41,16 +43,16 @@ final class SplashViewCoordinator: BaseCoordinator {
             persistenceService: userDefaultsPersistenceService
         )
         
+        let authenticationUseCase = AuthenticationUseCase()
         let getMyIdUseCase = GetMyIdUseCase(userRepository: userRespository)
         let getUserUseCase = GetUserUseCase(userRepository: userRespository)
-        let registerUserUseCase = RegisterUserUseCase(userRepository: userRespository)
         let globalFontUseCase = GlobalFontUseCase(globalFontRepository: globalFontRepository)
         
         let viewModel = SplashViewModel(
             sceneId: self.identifier,
+            authenticationUseCase: authenticationUseCase,
             getMyIdUseCase: getMyIdUseCase,
             getUserUseCase: getUserUseCase,
-            registerUserUseCase: registerUserUseCase,
             globalFontUseCase: globalFontUseCase
         )
 
@@ -59,6 +61,20 @@ final class SplashViewCoordinator: BaseCoordinator {
     }
     
     private func bind() {
+        NotificationCenter.default.publisher(for: Notifications.userNotLoggedIn, object: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.userNotLoggedIn()
+            }
+            .store(in: &self.cancellables)
+        
+        NotificationCenter.default.publisher(for: Notifications.userNotExists, object: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.userNotExists()
+            }
+            .store(in: &self.cancellables)
+        
         NotificationCenter.default.publisher(for: Notifications.userNotPaired, object: nil)
             .receive(on: DispatchQueue.main)
             .compactMap { $0.userInfo?[Keys.myId] as? DDID }
@@ -74,6 +90,22 @@ final class SplashViewCoordinator: BaseCoordinator {
                 self?.userAlreadyPaired(user: user)
             }
             .store(in: &self.cancellables)
+    }
+    
+    // FIXME: NOT IMPLEMENTED
+    private func userNotLoggedIn() {
+        // let identifier = UUID()
+        // let authenticationViewCoordinator = AuthenticationViewCoordinator()
+        // self.children[identifier] = authenticationViewCoordinator
+        // authenticationViewCoordinator.start()
+    }
+    
+    // FIXME: NOT IMPLEMENTED
+    private func userNotExists() {
+        // let identifier = UUID()
+        // let agreementViewCoordinator = AgreementViewCoordinator()
+        // self.children[identifier] = agreementViewCoordinator
+        // agreementViewCoordinator.start()
     }
     
     private func userNotPaired(myId: DDID) {
