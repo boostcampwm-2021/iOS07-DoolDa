8ce5ac760f86c78821f9d21de37c24ae6151b748
Jung Ji Seung <rlsmd9@naver.com>
2021-12-28 15:51:31 +0900

    (#508) ✨: 서비스 이용동의시 유저를 생성하도록 구현

--- Doolda/Doolda/AgreementScene/AgreementViewModel.swift
+++ Doolda/Doolda/AgreementScene/AgreementViewModel.swift
@@ -11,6 +11,7 @@ import Foundation
 protocol AgreementViewModelInput {
     func viewDidLoad()
     func agreementButtonDidTap()
+    func deinitRequested()
 }
 
 protocol AgreementViewModelOutput {
@@ -26,15 +27,43 @@ final class AgreementViewModel: AgreementViewModelProtocol {
     var serviceAgreementPublisher: AnyPublisher<String?, Never> { self.$serviceAgreement.eraseToAnyPublisher() }
     var privacyPolicyPublisher: AnyPublisher<String?, Never> { self.$privacyPolicy.eraseToAnyPublisher() }
     
+    private let sceneID: UUID
+    private let registerUserUseCase: RegisterUserUseCaseProtocol
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
     @Published private var error: Error?
     @Published private var serviceAgreement: String?
     @Published private var privacyPolicy: String?
+    
+    init(sceneId: UUID,
+         registerUserUseCase: RegisterUserUseCaseProtocol) {
+        self.sceneID = sceneId
+        self.registerUserUseCase = registerUserUseCase
+        bind()
+    }
     
     func viewDidLoad() {
         // FIXME: 서비스 정책 및 개인정보 정책에 관한 데이터 어떻게 로드할지 결정 후 작성
     }
     
     func agreementButtonDidTap() {
-        // FIXME: 코디네이터 생성 후 연결
+        self.registerUserUseCase.register()
+    }
+    
+    func deinitRequested() {
+        // FIXME: 코디네이터 구현 후 Notification 작성
+    }
+    
+    private func bind() {
+        self.registerUserUseCase.registeredUserPublisher
+            .compactMap { $0 }
+            .sink(receiveValue: { [weak self] in
+                // FIXME: 코디네이터 구현 후 Notification 작성
+            })
+            .store(in: &self.cancellables)
+
+        self.registerUserUseCase.errorPublisher
+            .assign(to: &$error)
     }
 }
