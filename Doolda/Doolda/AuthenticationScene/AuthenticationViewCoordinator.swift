912205b10289b1f2a7198a983b96eb55c24d9092
ehWld <729psw@gmail.com>
2021-12-28 19:01:32 +0900

    (#507) ✨: AuthenticationCoordinator enum 구현

--- Doolda/Doolda/AuthenticationScene/AuthenticationViewCoordinator.swift
+++ Doolda/Doolda/AuthenticationScene/AuthenticationViewCoordinator.swift
@@ -5,8 +5,34 @@
 //  Created by Dozzing on 2021/12/28.
 //
 
+import Combine
 import Foundation
+import UIKit
 
 final class AuthenticationViewCoordinator: BaseCoordinator {
-    
+
+    // MARK: - Nested enum
+
+    enum Notifications {
+        static let userDidSignIn = Notification.Name("userDidSignIn")
+    }
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    override init(identifier: UUID, presenter: UINavigationController) {
+        super.init(identifier: identifier, presenter: presenter)
+        self.bind()
+    }
+
+    func start() {
+
+    }
+
+    private func bind() {
+
+    }
+
+    private func userDidSignIn() {
+        
+    }
 }
