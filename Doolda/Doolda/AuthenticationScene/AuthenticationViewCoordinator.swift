c82011ccad31547c5a66152c378d9798b297a0da
ehWld <729psw@gmail.com>
2021-12-28 18:48:26 +0900

    (#506) ♻️: delegate 로직을 ViewModel로 이동

---
+++ Doolda/Doolda/AuthenticationScene/AuthenticationViewCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationViewCoordinator.swift
+//  Doolda
+//
+//  Created by Dozzing on 2021/12/28.
+//
+
+import Foundation
+
+final class AuthenticationViewCoordinator: BaseCoordinator {
+    
+}
