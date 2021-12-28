39de97a0ce875413e73506a03c47ce1a999e1ecf
yabby <yabby1997@gmail.com>
2021-12-28 17:57:32 +0900

    (#512) ✨: 애플 로그인에 따른 유저 검증 로직 변경 및 그에따라 영향받는 부분과 함께 전반적인 코드 리팩터링

--- Doolda/Doolda/SplashScene/SplashViewController.swift
+++ Doolda/Doolda/SplashScene/SplashViewController.swift
@@ -25,6 +25,7 @@ final class SplashViewController: UIViewController {
         var label = UILabel()
         label.text = "둘다"
         label.textColor = UIColor.dooldaLabel
+        label.font = UIFont(name: FontType.dovemayo.name, size: 72)
         label.textAlignment = .center
         return label
     }()
@@ -33,18 +34,19 @@ final class SplashViewController: UIViewController {
         var label = UILabel()
         label.text = "우리 둘만의 다이어리"
         label.textColor = UIColor.dooldaLabel
+        label.font = UIFont(name: FontType.dovemayo.name, size: 18)
         label.textAlignment = .center
         return label
     }()
 
     // MARK: - Private Properties
     
-    private var viewModel: SplashViewModel!
+    private var viewModel: SplashViewModelProtocol!
     private var cancellables: Set<AnyCancellable> = []
     
     // MARK: - Initializers
     
-    convenience init(viewModel: SplashViewModel) {
+    convenience init(viewModel: SplashViewModelProtocol) {
         self.init(nibName: nil, bundle: nil)
         self.viewModel = viewModel
     }
@@ -59,10 +61,8 @@ final class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
-        self.configureFont()
         self.bindUI()
-        self.viewModel?.applyGlobalFont()
-        self.viewModel?.prepareUserInfo()
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) { [weak self] in self?.viewModel.validateAccount() }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,6 +70,7 @@ final class SplashViewController: UIViewController {
     }
 
     // MARK: - Helpers
+    
     private func configureUI() {
         self.view.backgroundColor = .dooldaBackground
 
@@ -97,31 +98,21 @@ final class SplashViewController: UIViewController {
         }
     }
 
-    private func configureFont() {
-        self.titleLabel.font = UIFont(name: FontType.dovemayo.name, size: 72)
-        self.subtitleLabel.font = UIFont(name: FontType.dovemayo.name, size: 18)
-    }
-
     private func bindUI() {
-        self.viewModel?.$error
+        self.viewModel.errorPublisher
             .receive(on: DispatchQueue.main)
             .sink { [weak self] error in
                 guard error != nil else { return }
-                self?.presentNetworkAlert()
+                self?.showErrorAlert()
             }
             .store(in: &cancellables)
-        
-        NotificationCenter.default.publisher(for: GlobalFontUseCase.Notifications.globalFontDidSet, object: nil)
-            .sink { [weak self] _ in
-                self?.configureFont()
-            }
-            .store(in: &self.cancellables)
     }
 
     // MARK: - Private Methods
-    private func presentNetworkAlert() {
+    
+    private func showErrorAlert() {
         let alert = UIAlertController.networkAlert { _ in
-            self.viewModel?.prepareUserInfo()
+            self.viewModel.validateAccount()
         }
         self.present(alert, animated: true)
     }
--- Doolda/Doolda/SplashScene/SplashViewModel.swift
+++ Doolda/Doolda/SplashScene/SplashViewModel.swift
@@ -8,92 +8,44 @@
 import Combine
 import Foundation
 
-final class SplashViewModel {
-    @Published var error: Error?
+import FirebaseAuth
 
+protocol SplashViewModelInput {
+    func validateAccount()
+    func deinitRequested()
+}
+
+protocol SplashViewModelOutput {
+    var errorPublisher: AnyPublisher<Error?, Never> { get }
+}
+
+typealias SplashViewModelProtocol = SplashViewModelInput & SplashViewModelOutput
+
+final class SplashViewModel: SplashViewModelProtocol {
+    var errorPublisher: AnyPublisher<Error?, Never> { self.$error.eraseToAnyPublisher() }
+    
     private let sceneId: UUID
+    private let authenticationUseCase: AuthenticationUseCaseProtocol
     private let getMyIdUseCase: GetMyIdUseCaseProtocol
     private let getUserUseCase: GetUserUseCaseProtocol
-    private let registerUserUseCase: RegisterUserUseCaseProtocol
     private let globalFontUseCase: GlobalFontUseCaseProtocol
 
     private var cancellables: Set<AnyCancellable> = []
-    @Published private var user: User?
+    @Published private var error: Error?
     
     init(
         sceneId: UUID,
+        authenticationUseCase: AuthenticationUseCaseProtocol,
         getMyIdUseCase: GetMyIdUseCaseProtocol,
         getUserUseCase: GetUserUseCaseProtocol,
-        registerUserUseCase: RegisterUserUseCaseProtocol,
         globalFontUseCase: GlobalFontUseCaseProtocol
     ) {
         self.sceneId = sceneId
+        self.authenticationUseCase = authenticationUseCase
         self.getMyIdUseCase = getMyIdUseCase
         self.getUserUseCase = getUserUseCase
-        self.registerUserUseCase = registerUserUseCase
         self.globalFontUseCase = globalFontUseCase
-    }
-    
-    func applyGlobalFont() {
-        guard let globalFont = self.globalFontUseCase.getGlobalFont() else { return }
-        self.globalFontUseCase.setGlobalFont(with: globalFont.name)
-    }
-
-    func prepareUserInfo() {
-        self.bind()
-        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) { self.getMyId() }
-    }
-
-    private func bind() {
-        self.$user
-            .compactMap { $0 }
-            .sink(receiveValue: { [weak self] user in
-                if user.pairId?.ddidString.isEmpty == false {
-                    NotificationCenter.default.post(
-                        name: SplashViewCoordinator.Notifications.userAlreadyPaired,
-                        object: self,
-                        userInfo: [SplashViewCoordinator.Keys.user: user]
-                    )
-                } else {
-                    NotificationCenter.default.post(
-                        name: SplashViewCoordinator.Notifications.userNotPaired,
-                        object: self,
-                        userInfo: [SplashViewCoordinator.Keys.myId: user.id]
-                    )
-                }
-            })
-            .store(in: &self.cancellables)
-
-        self.registerUserUseCase.registeredUserPublisher
-            .compactMap { $0 }
-            .sink(receiveValue: { [weak self] in self?.user = $0 })
-            .store(in: &self.cancellables)
-
-        self.registerUserUseCase.errorPublisher
-            .assign(to: &$error)
-    }
-
-    private func getMyId() {
-        self.getMyIdUseCase.getMyId()
-            .sink(receiveValue: { [weak self] myId in
-                guard let myId = myId else {
-                    self?.registerUserUseCase.register()
-                    return
-                }
-                self?.getUser(with: myId)
-            })
-            .store(in: &self.cancellables)
-    }
-
-    private func getUser(with myId: DDID) {
-        self.getUserUseCase.getUser(for: myId)
-            .sink { [weak self] result in
-                guard case .failure(let error) = result else { return }
-                self?.error = error
-            } receiveValue: { [weak self] user in
-                self?.user = user
-            }
-            .store(in: &self.cancellables)
+        self.applyGlobalFont()
     }
     
     func deinitRequested() {
@@ -102,5 +54,66 @@ final class SplashViewModel {
             object: nil,
             userInfo: [BaseCoordinator.Keys.sceneId: self.sceneId]
         )
+    }
+    
+    func validateAccount() {
+        if let currentUser = self.authenticationUseCase.getCurrentUser() {
+            self.validateUserId(user: currentUser)
+        } else {
+            NotificationCenter.default.post(
+                name: SplashViewCoordinator.Notifications.userNotLoggedIn,
+                object: self
+            )
+        }
+    }
+    
+    private func validateUserId(user: FirebaseAuth.User) {
+        self.getMyIdUseCase.getMyId(for: user.uid)
+            .sink { [weak self] ddid in
+                if let userId = ddid {
+                    self?.validateAgreement(userId: userId)
+                } else {
+                    NotificationCenter.default.post(
+                        name: SplashViewCoordinator.Notifications.userNotExists,
+                        object: self
+                    )
+                }
+            }
+            .store(in: &self.cancellables)
+    }
+    
+    private func validateAgreement(userId: DDID) {
+        self.getUserUseCase.getUser(for: userId)
+            .sink { completion in
+                guard case .failure(let error) = completion else { return }
+                self.error = error
+            } receiveValue: { dooldaUser in
+                if let user = dooldaUser {
+                    if user.pairId?.ddidString.isEmpty == false {
+                        NotificationCenter.default.post(
+                            name: SplashViewCoordinator.Notifications.userAlreadyPaired,
+                            object: self,
+                            userInfo: [SplashViewCoordinator.Keys.user: user]
+                        )
+                    } else {
+                        NotificationCenter.default.post(
+                            name: SplashViewCoordinator.Notifications.userNotPaired,
+                            object: self,
+                            userInfo: [SplashViewCoordinator.Keys.myId: user.id]
+                        )
+                    }
+                } else {
+                    NotificationCenter.default.post(
+                        name: SplashViewCoordinator.Notifications.userNotExists,
+                        object: self
+                    )
+                }
+            }
+            .store(in: &self.cancellables)
+    }
+
+    private func applyGlobalFont() {
+        guard let globalFont = self.globalFontUseCase.getGlobalFont() else { return }
+        self.globalFontUseCase.setGlobalFont(with: globalFont.name)
     }
 }
