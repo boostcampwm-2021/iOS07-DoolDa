d2eb1c8b3d8e382d554eb4fa27aac0e2c1ff053f
Jung Ji Seung <rlsmd9@naver.com>
2022-03-09 22:55:45 +0900

    ✨: FirebaseNetworkService 구현

---
+++ Doolda/Doolda/Data/Interfaces/FirebaseNetworkServiceProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  FirebaseNetworkServiceProtocol.swift
+//  Doolda
+//
+//  Created by 정지승 on 2022/03/09.
+//
+
+import Combine
+import Foundation
+
+protocol FirebaseNetworkServiceProtocol {
+    func getDocument(collection: FirebaseCollection, document: String) -> AnyPublisher<[String: Any], Error>
+    func setDocument(collection: FirebaseCollection, document: String, dictionary: [String: Any]) -> AnyPublisher<Void, Error>
+    func getDocument<T: DataTransferable>(collection: FirebaseCollection, document: String) -> AnyPublisher<T, Error>
+    func setDocument<T: DataTransferable>(collection: FirebaseCollection, document: String, transferable: T) -> AnyPublisher<Void, Error>
+    func uploadData(path: String, fileName: String, data: Data) -> AnyPublisher<URL, Error>
+    func donwloadData(path: String, fileName: String) -> AnyPublisher<Data, Error>
+}
+
+protocol DataTransferable {
+    init?(dictionary: [String: Any])
+    var dictionary: [String: Any] { get }
+}
---
+++ Doolda/Doolda/Service/Networks/FirebaseNetworkService.swift
@@ -0,0 +1,102 @@
+//
+//  FirebaseNetworkService.swift
+//  Doolda
+//
+//  Created by 정지승 on 2022/03/09.
+//
+
+import Combine
+import Foundation
+
+import Firebase
+
+class FirebaseNetworkService: FirebaseNetworkServiceProtocol {
+    enum Errors: LocalizedError {
+        case invalidDocumentSnapshot
+        case snapshotNotDecodable
+        case dataUploadFailed
+        case dataDownloadFailed
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidDocumentSnapshot: return "올바르지 않은 스냅샷입니다."
+            case .snapshotNotDecodable: return "스냅샷 디코딩에 실패했습니다."
+            case .dataUploadFailed: return "데이터 업로드에 실패했습니다."
+            case .dataDownloadFailed: return "데이터 다운로드에 실패했습니다."
+            }
+        }
+    }
+    
+    func getDocument(collection: FirebaseCollection, document: String) -> AnyPublisher<[String: Any], Error> {
+        return Future { promise in
+            Firestore.firestore().collection(collection.rawValue)
+                .document(document)
+                .getDocument { snapshot, error in
+                    if let error = error { return promise(.failure(error)) }
+                    guard let dictionary = snapshot?.data() else { return promise(.failure(Errors.invalidDocumentSnapshot)) }
+                    return promise(.success(dictionary))
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func getDocument<T: DataTransferable>(collection: FirebaseCollection, document: String) -> AnyPublisher<T, Error> {
+        return getDocument(collection: collection, document: document)
+            .tryMap { dictionary in
+                guard let decoded = T(dictionary: dictionary) else { throw Errors.snapshotNotDecodable }
+                return decoded
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func setDocument(collection: FirebaseCollection, document: String, dictionary: [String: Any]) -> AnyPublisher<Void, Error> {
+        return Future { promise in
+            Firestore.firestore().collection(collection.rawValue)
+                .document(document)
+                .setData(dictionary) { error in
+                    if let error = error { return promise(.failure(error)) }
+                    return promise(.success(()))
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func setDocument<T: DataTransferable>(collection: FirebaseCollection, document: String, transferable: T) -> AnyPublisher<Void, Error> {
+        let dictionary = transferable.dictionary
+        return setDocument(collection: collection, document: document, dictionary: dictionary)
+    }
+    
+    func uploadData(path: String, fileName: String, data: Data) -> AnyPublisher<URL, Error> {
+        let dataPath = "/\(path)/\(fileName)"
+        let storage = Storage.storage().reference(withPath: dataPath)
+        
+        return Future<URL, Error> { promise in
+            storage.putData(data, metadata: nil) { _, error in
+                if let error = error { return promise(.failure(error)) }
+                storage.downloadURL { url, error in
+                    if let error = error { return promise(.failure(error)) }
+                    guard let url = url else { return promise(.failure(Errors.dataUploadFailed)) }
+                    return promise(.success(url))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func donwloadData(path: String, fileName: String) -> AnyPublisher<Data, Error> {
+        let dataPath = "/\(path)/\(fileName)"
+        let storage = Storage.storage().reference(withPath: dataPath)
+        
+        return Future<Data, Error> { promise in
+            storage.getData(maxSize: 1024 * 1024 * 1024) { data, error in
+                if let error = error { return promise(.failure(error)) }
+                guard let data = data else {
+                    return promise(.failure(Errors.dataDownloadFailed))
+                }
+
+                promise(.success(data))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
