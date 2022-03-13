9423a889cb87c199f0175067361f76dadefd4d4d
MINJUKIM <alsxm553@gmail.com>
2022-03-13 23:43:10 +0900

    ♻️: User가 DataTransferable을 채택하도록 구현

--- Doolda/Doolda/Domain/Entities/User.swift
+++ Doolda/Doolda/Domain/Entities/User.swift
@@ -24,3 +24,26 @@ struct User: Hashable {
         hasher.combine(friendId)
     }
 }
+
+extension User: DataTransferable {
+    init?(dictionary: [String : Any]) {
+        guard let id = dictionary["id"] as? String,
+              let pairId = dictionary["pairId"] as? String,
+              let friendId = dictionary["friendId"] as? String,
+              let ddid = DDID(from: id),
+              let pairDDID = DDID(from: pairId),
+              let friendDDID = DDID(from: friendId) else { return nil }
+        
+        self.id = ddid
+        self.pairId = pairDDID
+        self.friendId = friendDDID
+    }
+    
+    var dictionary: [String : Any] {
+        return [
+            "id": self.id.ddidString,
+            "pairId": self.pairId?.ddidString ?? "",
+            "friendId": self.friendId?.ddidString ?? ""
+        ]
+    }
+}
