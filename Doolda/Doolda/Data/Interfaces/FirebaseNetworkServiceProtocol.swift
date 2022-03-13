cefef64ab6877c6f191794573f3b6d60fa4633a4
도찡 <729psw@gmail.com>
2022-03-13 20:46:00 +0900

    ✨: firebaseNetworkService에 deleteDocument 추가 및 싱글톤 객체 선언

--- Doolda/Doolda/Data/Interfaces/FirebaseNetworkServiceProtocol.swift
+++ Doolda/Doolda/Data/Interfaces/FirebaseNetworkServiceProtocol.swift
@@ -13,6 +13,7 @@ protocol FirebaseNetworkServiceProtocol {
     func setDocument(collection: FirebaseCollection, document: String, dictionary: [String: Any]) -> AnyPublisher<Void, Error>
     func getDocument<T: DataTransferable>(collection: FirebaseCollection, document: String) -> AnyPublisher<T, Error>
     func setDocument<T: DataTransferable>(collection: FirebaseCollection, document: String, transferable: T) -> AnyPublisher<Void, Error>
+    func deleteDocument(collection: FirebaseCollection, document: String) -> AnyPublisher<Void, Error>
     func uploadData(path: String, fileName: String, data: Data) -> AnyPublisher<URL, Error>
     func donwloadData(path: String, fileName: String) -> AnyPublisher<Data, Error>
 }
