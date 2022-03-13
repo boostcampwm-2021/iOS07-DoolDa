faa711a5da18fdb4eb38005d5f123cf1c83a01a3
Jung Ji Seung <rlsmd9@naver.com>
2022-03-13 14:43:33 +0900

    (#521) ♻️: PageEntity가 DataTransferable을 채택하도록 구현

--- Doolda/Doolda/Domain/Entities/PageEntity.swift
+++ Doolda/Doolda/Domain/Entities/PageEntity.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+import Firebase
+
 struct PageEntity: Hashable {
     let author: User
     let createdTime: Date
@@ -18,5 +20,32 @@ struct PageEntity: Hashable {
         hasher.combine(createdTime)
         hasher.combine(updatedTime)
         hasher.combine(jsonPath)
+    }
+}
+
+extension PageEntity: DataTransferable {
+    init?(dictionary: [String : Any]) {
+        guard let authorId = dictionary["author"] as? String,
+              let pairId = dictionary["pairId"] as? String,
+              let authorDDID = DDID(from: authorId),
+              let pairDDID = DDID(from: pairId),
+              let createdTime = dictionary["createdTime"] as? Timestamp,
+              let updatedTime = dictionary["updatedTime"] as? Timestamp,
+              let jsonPath = dictionary["jsonPath"] as? String else { return nil }
+        
+        self.author = User(id: authorDDID, pairId: pairDDID)
+        self.createdTime = createdTime.dateValue()
+        self.updatedTime = updatedTime.dateValue()
+        self.jsonPath = jsonPath
+    }
+    
+    var dictionary: [String : Any] {
+        return [
+            "author": self.author.id.ddidString,
+            "createdTime": self.createdTime,
+            "updatedTime": self.updatedTime,
+            "jsonPath": self.jsonPath,
+            "pairId": self.author.pairId?.ddidString ?? ""
+        ]
     }
 }
