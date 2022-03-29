21a70b9f7a881b185613611ab520e78badda087f
도찡 <729psw@gmail.com>
2022-03-30 01:28:54 +0900

    ✨: AuthenticationViewCoordinator 구현

--- Doolda/Doolda/AuthenticationScene/AuthenticationViewCoordinator.swift
+++ Doolda/Doolda/AuthenticationScene/AuthenticationViewCoordinator.swift
@@ -5,8 +5,44 @@
 //  Created by Dozzing on 2022/03/30.
 //
 
+import Combine
 import Foundation
+import UIKit
 
 final class AuthenticationViewCoordinator: BaseCoordinator {
-    
+
+    // MARK: - Nested enum
+
+    enum Notifications {
+        static let userDidSignIn = Notification.Name("userDidSignIn")
+    }
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    override init(identifier: UUID, presenter: UINavigationController) {
+        super.init(identifier: identifier, presenter: presenter)
+        self.bind()
+    }
+
+    func start() {
+        let authenticationUseCase = AuthenticationUseCase()
+        let viewModel = AuthenticationViewModel(sceneId: self.identifier, authenticationUseCase: authenticationUseCase)
+        let viewController = AuthenticationViewController(viewModel: viewModel)
+        self.presenter.pushViewController(viewController, animated: false)
+    }
+
+    private func bind() {
+        NotificationCenter.default.publisher(for: Notifications.userDidSignIn, object: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.userDidSignIn()
+            }
+            .store(in: &self.cancellables)
+    }
+
+    private func userDidSignIn() {
+        // FIXME: 미구현
+        print("Present Agreement ViewController")
+    }
 }
+
