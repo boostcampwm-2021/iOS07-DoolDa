403341df253f1586716702946049e66d7828a867
MINJU KIM <minju.kim.620@linecorp.com>
2022-05-16 00:37:15 +0900

    (#552) ✨: SignUpUseCase 구현

---
+++ Doolda/Doolda/Domain/UseCases/SignUpUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpUseCase.swift
+//  Doolda
+//
+//  Created by user on 2022/05/16.
+//
+
+import Combine
+import Foundation
+
+import FirebaseAuth
+
+
+final class SignUpUseCase: SignUpUseCaseProtocol {
+    func signUp(email: String, password: String, completion: ((AuthDataResult?, Error?) -> Void)?) {
+        Auth.auth().createUser(withEmail: email, password: password)
+    }
+}
--- Doolda/Doolda/SignUpScene/SignUpViewCoordinator.swift
+++ Doolda/Doolda/SignUpScene/SignUpViewCoordinator.swift
@@ -19,7 +19,8 @@ final class SignUpViewCoordinator: BaseCoordinator {
     }
 
     func start() {
-        let viewModel = SignUpViewModel()
+        let singUpUseCase = SignUpUseCase()
+        let viewModel = SignUpViewModel(signUpUseCase: singUpUseCase)
         let viewController = SignUpViewController(viewModel: viewModel)
         self.presenter.pushViewController(viewController, animated: false)
     }
