eb444c21c6b95f3a03e8046f4674c4fed33e749a
yabby <yabby1997@gmail.com>
2022-06-04 10:47:16 +0900

    (#570) ♻️: PushNotification -> Notification 제거

--- Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
+++ Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
@@ -12,34 +12,29 @@ struct PushMessageEntity {
     let body: String
     let data: [String: String]
     
+    init(title: String, body: String, data: [String: String] = [:]) {
+        self.title = title
+        self.body = body
+        self.data = data
+    }
+    
     static let userPairedWithFriend: PushMessageEntity = PushMessageEntity(
         title: "똑똑! 🙋‍♀️🙋‍♂️",
-        body: "누군가가 당신을 친구로 연결했어요!",
-        data: ["notification": "userPairedWithFriend"]
+        body: "누군가가 당신을 친구로 연결했어요!"
     )
     
     static let userPostedNewPage: PushMessageEntity = PushMessageEntity(
         title: "띵동! 🔔",
-        body: "친구가 다이어리를 작성했어요!\n새 다이어리를 확인해볼까요?",
-        data: ["notification": "userPostedNewPage"]
+        body: "친구가 다이어리를 작성했어요!\n새 다이어리를 확인해볼까요?"
     )
     
     static let userRequestedNewPage: PushMessageEntity = PushMessageEntity(
         title: "쿡쿡! 🥺👉🏻👉🏻",
-        body: "친구가 다이어리 작성을 기다리고있어요.\n새 다이어리를 작성해볼까요?",
-        data: ["notification": "userRequestedNewPage"]
+        body: "친구가 다이어리 작성을 기다리고있어요.\n새 다이어리를 작성해볼까요?"
     )
     
     static let userDisconnected: PushMessageEntity = PushMessageEntity(
         title: "연결 해제",
-        body: "상대방이 연결을 해제했습니다.",
-        data: ["notification": "userDisconnected"]
+        body: "상대방이 연결을 해제했습니다."
     )
-    
-    enum Notifications {
-        static let userPairedWithFriend = Notification.Name("userPairedWithFriend")
-        static let userPostedNewPage = Notification.Name("userPostedNewPage")
-        static let userRequestedNewPage = Notification.Name("userRequestedNewPage")
-        static let userDisconnected = Notification.Name("userDisconnected")
-    }
 }
