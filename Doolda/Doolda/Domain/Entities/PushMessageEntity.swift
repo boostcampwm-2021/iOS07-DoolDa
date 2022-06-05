7500a719b58b94c62b76351e61473ab313fcd8f0
yabby <yabby1997@gmail.com>
2022-06-05 18:08:25 +0900

    (#570) ♻️: 연결해제 notification 탭 시 앱 재시작하도록 변경

--- Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
+++ Doolda/Doolda/Domain/Entities/PushMessageEntity.swift
@@ -26,33 +26,36 @@ struct PushMessageEntity {
     static let userPostedNewPage: PushMessageEntity = PushMessageEntity(
         title: "띵동! 🔔",
         body: "친구가 다이어리를 작성했어요!\n새 다이어리를 확인해볼까요?",
-        data: [DataKey.event: DataValue.userPostedNewPage]
+        data: [DataKey.event: EventIdentifier.userPostedNewPage]
     )
     
     static let userRequestedNewPage: PushMessageEntity = PushMessageEntity(
         title: "쿡쿡! 🥺👉🏻👉🏻",
         body: "친구가 다이어리 작성을 기다리고있어요.\n새 다이어리를 작성해볼까요?",
-        data: [DataKey.event: DataValue.userRequestedNewPage]
+        data: [DataKey.event: EventIdentifier.userRequestedNewPage]
     )
     
     static let userDisconnected: PushMessageEntity = PushMessageEntity(
         title: "연결 해제",
-        body: "상대방이 연결을 해제했습니다."
+        body: "상대방이 연결을 해제했습니다.",
+        data: [DataKey.event: EventIdentifier.userDisconnected]
     )
     
     enum DataKey {
         static let event = "event"
     }
     
-    enum DataValue {
+    enum EventIdentifier {
         static let userPostedNewPage = "userPostedNewPage"
         static let userRequestedNewPage = "userRequestedNewPage"
+        static let userDisconnected = "userDisconnected"
     }
     
     enum Notifications {
         static let dict: [String: Notification.Name] = [
-            DataValue.userPostedNewPage: didReceiveUserPostedNewPageEvent,
-            DataValue.userRequestedNewPage: didReceiveUserRequestedNewPageEvent
+            EventIdentifier.userPostedNewPage: didReceiveUserPostedNewPageEvent,
+            EventIdentifier.userRequestedNewPage: didReceiveUserRequestedNewPageEvent,
+            EventIdentifier.userDisconnected: AppCoordinator.Notifications.appRestartSignal
         ]
         
         static let didReceiveUserPostedNewPageEvent = Notification.Name(rawValue: "didReceiveUserPostedNewPageEvent")
