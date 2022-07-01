b1119638dc2d81982aed5a5c2b17b1fb89dcb3ba
도찡 <729psw@gmail.com>
2022-07-01 19:30:10 +0900

    (#602) ✨: 존재하지 않는 페이지의 metaData를 지워주는 로직 추가

--- Doolda/Doolda/Domain/UseCases/GetRawPageUseCase.swift
+++ Doolda/Doolda/Domain/UseCases/GetRawPageUseCase.swift
@@ -7,15 +7,32 @@
 
 import Combine
 import Foundation
+import FirebaseStorage
 
 final class GetRawPageUseCase: GetRawPageUseCaseProtocol {
     private let rawPageRepository: RawPageRepositoryProtocol
+    private let pageRepository: PageRepositoryProtocol
     
-    init(rawPageRepository: RawPageRepositoryProtocol) {
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(rawPageRepository: RawPageRepositoryProtocol, pageRepository: PageRepositoryProtocol) {
         self.rawPageRepository = rawPageRepository
+        self.pageRepository = pageRepository
     }
     
     func getRawPageEntity(metaData: PageEntity) -> AnyPublisher<RawPageEntity, Error> {
         return self.rawPageRepository.fetch(metaData: metaData)
+            .catch { [weak self] error -> AnyPublisher<RawPageEntity, Error> in
+                let storageError = StorageErrorCode(rawValue: (error as NSError).code)
+                if storageError == .objectNotFound { self?.deleteDummyPage(for: metaData) }
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func deleteDummyPage(for page: PageEntity) {
+        self.pageRepository.deletePage(for: page)
+            .sink (receiveCompletion: { _ in }, receiveValue: { _ in })
+            .store(in: &self.cancellables)
     }
 }
